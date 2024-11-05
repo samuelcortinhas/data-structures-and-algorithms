0d99c2e435a035ebfd56a229c8c7c798d7efb220
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2024-11-05 16:56:06 +0000

    implement basic recursive binary search

---
+++ core_skills/algorithms/recursive_binary_search.py
@@ -0,0 +1,20 @@
+def recursive_binary_search(lst, target):
+    """Assumes list is sorted"""
+    if len(lst) == 0:
+        return False
+
+    midpoint = (len(lst) - 1) // 2
+
+    if lst[midpoint] == target:
+        return True
+    elif lst[midpoint] < target:
+        return recursive_binary_search(lst[midpoint + 1 :], target)
+    else:
+        return recursive_binary_search(lst[:midpoint], target)
+
+
+if __name__ == "__main__":
+    lst = range(10)
+
+    print(recursive_binary_search(lst, 12))
+    print(recursive_binary_search(lst, 6))
