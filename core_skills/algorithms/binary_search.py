3003ef4ac96bc79e0fc96560947d1edc1d3b73bf
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2024-11-05 16:34:51 +0000

    implement simple binary search

---
+++ core_skills/algorithms/binary_search.py
@@ -0,0 +1,22 @@
+def binary_search(lst, target):
+    """Assumes list is sorted"""
+    start = 0
+    end = len(lst) - 1
+
+    while start <= end:
+        midpoint = (start + end) // 2
+
+        if lst[midpoint] == target:
+            return midpoint
+
+        elif lst[midpoint] < target:
+            start = midpoint + 1
+
+        else:
+            end = midpoint - 1
+
+if __name__ == "__main__":
+    lst = range(10)
+
+    print(binary_search(lst, 12))
+    print(binary_search(lst, 6))
