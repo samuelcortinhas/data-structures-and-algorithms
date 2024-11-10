8726bf58eebc5ef67e4baa86e94ba14c02537e6d
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2024-11-10 17:54:13 +0000

    my attempt at linked list

--- core_skills/data_structures/linked_list.py
+++ core_skills/data_structures/linked_list.py
@@ -14,5 +14,27 @@ class Node:
         return "<Node data: {}>".format(self.data)
 
 
+class LinkedList:
+    """
+    A linked list made of nodes.
+    Each node stores some data and a reference to the next node in the list.
+    """
+
+    linked_list = []
+
+    def __init__(self, lst):
+        lst.reverse()
+        for x in lst:
+            node = Node(x)
+            if self.linked_list:
+                node.self_node = self.linked_list[-1]
+            self.linked_list.append(node)
+        self.linked_list.reverse()
+
+    def __repr__(self):
+        return "<Linked list: {}>".format(self.linked_list)
+
+
 if __name__ == "__main__":
     print(Node(5))
+    print(LinkedList([1, 2, 3]))
