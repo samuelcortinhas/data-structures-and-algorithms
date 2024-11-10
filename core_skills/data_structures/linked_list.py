775c5701cac2a9d6cef6959d77c44e360cc3aced
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2024-11-10 21:49:43 +0000

    add init

--- core_skills/data_structures/linked_list.py
+++ core_skills/data_structures/linked_list.py
@@ -18,21 +18,9 @@ class LinkedList:
     Each node stores some data and a reference to the next node in the list.
     """
 
-    linked_list = []
-
-    def __init__(self, lst):
-        lst.reverse()
-        for x in lst:
-            node = Node(x)
-            if self.linked_list:
-                node.self_node = self.linked_list[-1]
-            self.linked_list.append(node)
-        self.linked_list.reverse()
-
-    def __repr__(self):
-        return "<Linked list: {}>".format(self.linked_list)
+    def __init__(self):
+        self.head = None
 
 
 if __name__ == "__main__":
     print(Node(5))
-    print(LinkedList([1, 2, 3]))
