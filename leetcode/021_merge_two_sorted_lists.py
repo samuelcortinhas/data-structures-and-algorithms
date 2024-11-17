6d8ec7dbfc1fb1b1071a9ca9764551dabf84ef89
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2024-11-17 14:52:42 +0000

    merge two sorted linked lists

---
+++ leetcode/021_merge_two_sorted_lists.py
@@ -0,0 +1,33 @@
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: Optional[ListNode]
+        :type l2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        # Time O(n log n), Memory O(n)
+        current = ListNode()  # fake head
+        head = current
+
+        while l1 and l2:
+            if l1.val <= l2.val:
+                current.next = l1
+                l1 = l1.next
+            else:
+                current.next = l2
+                l2 = l2.next
+
+            current = current.next
+
+        if l1:
+            current.next = l1
+        elif l2:
+            current.next = l2
+
+        return head.next
