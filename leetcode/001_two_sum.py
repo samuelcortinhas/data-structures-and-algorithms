58029cac7eed85d11130c16d4866ce19b0edad00
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2024-11-18 23:04:54 +0000

    fast hashmap solution

--- leetcode/001_two_sum.py
+++ leetcode/001_two_sum.py
@@ -5,8 +5,19 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        # Time O(n^2), Memory O(1)
+        # Time O(n), Memory O(n)
+        hashmap = {v: i for i, v in enumerate(nums)}
+
         for i, x in enumerate(nums):
-            for j, y in enumerate(nums[i + 1 :]):
-                if x + y == target:
-                    return [i, j + i + 1]
+            diff = target - x
+
+            j = hashmap.get(diff)
+            if j and j != i:
+                return [i, j]
+
+    # def twoSumBruteForce(self, nums, target):
+    #     # Time O(n^2), Memory O(1)
+    #     for i, x in enumerate(nums):
+    #         for j, y in enumerate(nums[i + 1 :]):
+    #             if x + y == target:
+    #                 return [i, j + i + 1]
