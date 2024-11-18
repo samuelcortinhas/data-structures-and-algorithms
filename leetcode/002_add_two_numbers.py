f039273e8ea3e179c4004b5f0253fa1647333d50
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2024-11-18 22:32:09 +0000

    optimise solution

--- leetcode/002_add_two_numbers.py
+++ leetcode/002_add_two_numbers.py
@@ -13,44 +13,26 @@ class Solution(object):
         :rtype: Optional[ListNode]
         """
         # Time O(n), Memory O(n) where n is max length of two linked lists
-        a1 = []
-        a2 = []
-
-        while l1:
-            a1.append(l1.val)
-            l1 = l1.next
-
-        while l2:
-            a2.append(l2.val)
-            l2 = l2.next
-
-        i = len(a1)
-        j = len(a2)
-
-        out = []
-
-        if i > j:
-            a2 = a2 + [0] * (i - j)
-        elif j > i:
-            a1 = a1 + [0] * (j - i)
-
+        head = current = ListNode(0)
         rem = 0
-        for x, y in zip(a1, a2):
-            if x + y + rem >= 10:
-                out.append(x + y - 10 + rem)
+
+        while l1 or l2 or rem:
+            v = rem
+            if l1:
+                v += l1.val
+                l1 = l1.next
+
+            if l2:
+                v += l2.val
+                l2 = l2.next
+
+            if v >= 10:
+                current.next = ListNode(v - 10)
                 rem = 1
             else:
-                out.append(x + y + rem)
+                current.next = ListNode(v)
                 rem = 0
 
-        if rem == 1:
-            out.append(1)
-
-        head = ListNode(out[0])
-        current = head
-
-        for u in out[1:]:
-            current.next = ListNode(u)
             current = current.next
 
-        return head
+        return head.next
