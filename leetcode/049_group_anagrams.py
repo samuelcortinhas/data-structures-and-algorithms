a0b875ec2c1f0582a4f46fe15d9d3dc6ccc213d4
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2024-11-25 17:53:28 +0000

    compressed code for signature

--- leetcode/049_group_anagrams.py
+++ leetcode/049_group_anagrams.py
@@ -4,17 +4,13 @@ from collections import Counter, defaultdict
 class Solution(object):
     def getAnagramSignature(self, chars):
         """
-        Computes an anagram invariant signature on a string of characters
+        Compute an anagram invariant signature on string of characters
         """
-        # Time O(m), Memory O(log(m))
-        c = Counter(chars)
-        sig = ""
-        for i in range(26):
-            k = chr(ord("a") + i)
-            v = c.get(k, 0)
-            if v > 0:
-                sig = sig + "{}{}".format(k, str(v))
-        return sig
+        # Time O(m), Memory O(1)
+        counts = [0] * 26
+        for x in chars:
+            counts[ord(x) - ord("a")] += 1
+        return tuple(counts)  # tuple is hashable and valid as dict key
 
     def groupAnagrams(self, strs):
         """
@@ -27,6 +23,20 @@ class Solution(object):
             sig = self.getAnagramSignature(x)
             seen[sig].append(x)
         return list(seen.values())
+
+    # def getAnagramSignatureShorter(self, chars):
+    #     """
+    #     Computes an anagram invariant signature on a string of characters
+    #     """
+    #     # Time O(m), Memory O(log(m))
+    #     c = Counter(chars)
+    #     sig = ""
+    #     for i in range(26):
+    #         k = chr(ord("a") + i)
+    #         v = c.get(k, 0)
+    #         if v > 0:
+    #             sig = sig + "{}{}".format(k, str(v))
+    #     return sig
 
     # def groupAnagrams(self, strs):
     #     """
