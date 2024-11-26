00994c985fdf0e1dbf8bd676c698d7e3ee2177b6
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2024-11-26 16:51:03 +0000

    use bucket sort

--- leetcode/347_top_k_frequent_elements.py
+++ leetcode/347_top_k_frequent_elements.py
@@ -1,4 +1,5 @@
 from collections import Counter
+from itertools import chain, islice
 
 
 class Solution(object):
@@ -8,11 +9,24 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
-        # Time O(n log n), Memory O(n)
+        # "Bucket sort"
+        # Time O(n), Memory O(n)
         counts = Counter(nums)
-        l = list(counts.items())
-        l.sort(key=lambda x: x[1], reverse=True)
-        return [u[0] for u in l[:k]]
+
+        freqs = [[] for _ in range(len(nums) + 1)]
+        for n, v in counts.items():
+            freqs[v].append(n)
+
+        top = chain(*freqs[::-1])
+        topk = islice(top, k)
+        return list(topk)
+
+    # def topKFrequentDirect(self, nums, k):
+    #     # Time O(n log n), Memory O(n)
+    #     counts = Counter(nums)
+    #     l = list(counts.items())
+    #     l.sort(key=lambda x: x[1], reverse=True)
+    #     return [u[0] for u in l[:k]]
 
 
 if __name__ == "__main__":
