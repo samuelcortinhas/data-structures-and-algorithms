d29ce3807c298d9a515d5c0b8d59de15a9663af3
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2024-11-30 21:11:14 +0000

    memory optimised 3 sum

--- leetcode/015_three_sum.py
+++ leetcode/015_three_sum.py
@@ -3,25 +3,24 @@ from typing import List
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        # Time O(n^2), Memory O(n)
+        # Time O(n^2), Memory O(1)
         nums.sort()
         triples = []
 
-        outer_seen = set()
         for i, n in enumerate(nums):
             if n > 0:
                 return triples
 
-            if n in outer_seen:
+            if i > 0 and n == nums[i - 1]:
                 continue
-            outer_seen.add(n)
 
             # solve 2sum II
-            inner_seen = set()
+            prev = nums[0] - 1
             target = -n
             j, k = i + 1, len(nums) - 1
             while j < k:
-                if nums[j] in inner_seen:
+                if nums[j] == prev:
+                    prev = nums[j]
                     j += 1
                     continue
 
@@ -29,10 +28,45 @@ class Solution:
                 if s > target:
                     k -= 1
                 elif s < target:
-                    inner_seen.add(nums[j])
+                    prev = nums[j]
                     j += 1
                 else:
                     triples.append([nums[i], nums[j], nums[k]])
-                    inner_seen.add(nums[j])
+                    prev = nums[j]
                     j += 1
         return triples
+
+    # def threeSumV1(self, nums: List[int]) -> List[List[int]]:
+    #     # Time O(n^2), Memory O(n)
+    #     nums.sort()
+    #     triples = []
+
+    #     outer_seen = set()
+    #     for i, n in enumerate(nums):
+    #         if n > 0:
+    #             return triples
+
+    #         if n in outer_seen:
+    #             continue
+    #         outer_seen.add(n)
+
+    #         # solve 2sum II
+    #         inner_seen = set()
+    #         target = -n
+    #         j, k = i + 1, len(nums) - 1
+    #         while j < k:
+    #             if nums[j] in inner_seen:
+    #                 j += 1
+    #                 continue
+
+    #             s = nums[j] + nums[k]
+    #             if s > target:
+    #                 k -= 1
+    #             elif s < target:
+    #                 inner_seen.add(nums[j])
+    #                 j += 1
+    #             else:
+    #                 triples.append([nums[i], nums[j], nums[k]])
+    #                 inner_seen.add(nums[j])
+    #                 j += 1
+    #     return triples
