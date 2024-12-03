61fb7b4d7eefa6120954e91b19847cd92febac1f
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2024-12-03 21:32:50 +0000

    implement insertion sort

---
+++ core_skills/algorithms/insertion_sort.py
@@ -0,0 +1,15 @@
+def insertion_sort(arr):
+    # Time O(n^2), Memory O(1), stable
+    for i in range(len(arr)):
+        j = i
+        while j >= 0 and arr[j - 1] > arr[j]:
+            tmp = arr[j]
+            arr[j] = arr[j - 1]
+            arr[j - 1] = tmp
+            j -= 1
+    return arr
+
+
+if __name__ == "__main__":
+    arr = [8, 1, 7, 8, 4, 2, 7, 1, 8, 3]
+    print(insertion_sort(arr))
