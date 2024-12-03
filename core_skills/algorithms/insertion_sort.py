0c420afc58209eaa94f7dec9279b8de29c74b366
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2024-12-03 21:40:37 +0000

    small fix

--- core_skills/algorithms/insertion_sort.py
+++ core_skills/algorithms/insertion_sort.py
@@ -2,7 +2,7 @@ def insertion_sort(arr):
     # Time O(n^2), Memory O(1), stable
     for i in range(len(arr)):
         j = i
-        while j >= 0 and arr[j - 1] > arr[j]:
+        while j >= 1 and arr[j - 1] > arr[j]:
             tmp = arr[j]
             arr[j] = arr[j - 1]
             arr[j - 1] = tmp
