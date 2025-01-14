583447ac50940f8cb534b5b2205123fe722cbc58
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-01-14 22:42:38 +0000

    optimal solution

--- leetcode/121_best_time_to_buy_and_sell_stock.py
+++ leetcode/121_best_time_to_buy_and_sell_stock.py
@@ -2,12 +2,25 @@ from typing import List
 
 
 class Solution:
-    def maxProfitInitial(self, prices: List[int]) -> int:
-        # Time O(n^2), Memory O(1)
+    def maxProfit(self, prices: List[int]) -> int:
+        # Time O(n), Memory O(1)
         max_profit = 0
-        for i in range(len(prices)):
-            for j in range(i + 1, len(prices)):
+        i, j = 0, 1
+        while j < len(prices):
+            if prices[i] < prices[j]:
                 profit = prices[j] - prices[i]
-                if profit > max_profit:
-                    max_profit = profit
+                max_profit = max(max_profit, profit)
+            else:
+                i = j
+            j += 1
         return max_profit
+
+    # def maxProfitInitial(self, prices: List[int]) -> int:
+    #     # Time O(n^2), Memory O(1)
+    #     max_profit = 0
+    #     for i in range(len(prices)):
+    #         for j in range(i + 1, len(prices)):
+    #             profit = prices[j] - prices[i]
+    #             if profit > max_profit:
+    #                 max_profit = profit
+    #     return max_profit
