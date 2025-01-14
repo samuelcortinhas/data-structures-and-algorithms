d03ab395402cfb108224af2d5ea4b47792a57fd9
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-01-14 23:07:54 +0000

    solve binary search

---
+++ leetcode/704_binary_search.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        # Time O(log(n)), Memory O(1)
+        i, j = 0, len(nums) - 1
+
+        while i <= j:
+            mid = (i + j) // 2
+            print(i, j, mid)
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                i = mid + 1
+            else:
+                j = mid - 1
+
+        return -1
