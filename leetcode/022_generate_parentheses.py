a88954b68ea250036b59ea03f3668f7d41eabc7f
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-01-16 15:50:16 +0000

    first working attemot

---
+++ leetcode/022_generate_parentheses.py
@@ -0,0 +1,34 @@
+from typing import List
+
+
+class Solution:
+    def isvalid(self, s, n):
+        stack = []
+        for c in s:
+            if c == "(":
+                stack.append(c)
+            elif len(stack) == 0:
+                return False
+            elif stack[-1] == "(":
+                stack.pop()
+
+        return (")" not in stack) and (
+            sum([1 for i in stack if i == "("]) + len(s) <= 2 * n
+        )
+
+    def generateParenthesisV1(self, n: int) -> List[str]:
+        stacks = ["("]
+        new_stacks = []
+
+        for i in range(2 * n - 1):
+            for stack in stacks:
+                if self.isvalid(stack + "(", n):
+                    new_stacks.append(stack + "(")
+
+                if self.isvalid(stack + ")", n):
+                    new_stacks.append(stack + ")")
+
+            stacks = new_stacks
+            new_stacks = []
+
+        return stacks
