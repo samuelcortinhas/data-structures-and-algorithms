3b21ef4630dd6a25c47012b3eeae337722db70b2
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-01-16 17:22:13 +0000

    use min stack along with index

---
+++ leetcode/739_daily_temperatures.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        # Time O(n), Memory O(n)
+        answer = [0] * len(temperatures)
+        stack = [(temperatures[0], 0)]
+
+        i = 1
+        while i < len(temperatures):
+            while stack and (temperatures[i] > stack[-1][0]):
+                answer[stack[-1][1]] = i - stack[-1][1]
+                stack.pop()
+
+            stack.append((temperatures[i], i))
+            i += 1
+
+        return answer
