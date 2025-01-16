e2a66bfb4ce6460a8fda45ecd71da0370c28f840
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-01-16 13:35:53 +0000

    two pointer solution

--- leetcode/011_container_with_most_water.py
+++ leetcode/011_container_with_most_water.py
@@ -2,13 +2,30 @@ from typing import List
 
 
 class Solution:
-    def maxAreaBruteForce(self, height: List[int]) -> int:
-        # Time O(n^2), Memory O(1)
-        largest_area = 0
-        for i, h in enumerate(height):
-            j = i + 1
-            while j < len(height):
-                area = (j - i) * min(h, height[j])
-                largest_area = max(area, largest_area)
-                j += 1
-        return largest_area
+    def maxArea(self, height: List[int]) -> int:
+        # Time O(n), Memory O(1)
+        max_area = 0
+        left, right = 0, len(height) - 1
+        min_edge_height = 0
+        while left < right:
+            if min(height[left], height[right]) > min_edge_height:
+                area = (right - left) * min(height[left], height[right])
+                max_area = max(area, max_area)
+
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return max_area
+
+    # def maxAreaBruteForce(self, height: List[int]) -> int:
+    #     # Time O(n^2), Memory O(1)
+    #     largest_area = 0
+    #     for i, h in enumerate(height):
+    #         j = i + 1
+    #         while j < len(height):
+    #             area = (j - i) * min(h, height[j])
+    #             largest_area = max(area, largest_area)
+    #             j += 1
+    #     return largest_area
