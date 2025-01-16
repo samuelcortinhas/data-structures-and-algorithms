5c30a6d4ed2de88052fb65b68a07ffdb762dc2fa
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-01-16 17:31:56 +0000

    small optimisations

--- leetcode/739_daily_temperatures.py
+++ leetcode/739_daily_temperatures.py
@@ -5,15 +5,26 @@ class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         # Time O(n), Memory O(n)
         answer = [0] * len(temperatures)
-        stack = [(temperatures[0], 0)]
-
-        i = 1
-        while i < len(temperatures):
-            while stack and (temperatures[i] > stack[-1][0]):
+        stack = []
+        for i, t in enumerate(temperatures):
+            while stack and (t > stack[-1][0]):
                 answer[stack[-1][1]] = i - stack[-1][1]
                 stack.pop()
-
-            stack.append((temperatures[i], i))
-            i += 1
-
+            stack.append((t, i))
         return answer
+
+    # def dailyTemperaturesV1(self, temperatures: List[int]) -> List[int]:
+    #     # Time O(n), Memory O(n)
+    #     answer = [0] * len(temperatures)
+    #     stack = [(temperatures[0], 0)]
+
+    #     i = 1
+    #     while i < len(temperatures):
+    #         while stack and (temperatures[i] > stack[-1][0]):
+    #             answer[stack[-1][1]] = i - stack[-1][1]
+    #             stack.pop()
+
+    #         stack.append((temperatures[i], i))
+    #         i += 1
+
+    #     return answer
