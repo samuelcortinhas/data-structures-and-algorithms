ad5e48e1b439b46498e52938637d51f3af6c61cf
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-01-17 17:31:44 +0000

    more elegant solution

--- leetcode/002_add_two_numbers.py
+++ leetcode/002_add_two_numbers.py
@@ -1,3 +1,6 @@
+from typing import Optional
+
+
 # Definition for singly-linked list.
 class ListNode(object):
     def __init__(self, val=0, next=None):
@@ -6,33 +9,56 @@ class ListNode(object):
 
 
 class Solution(object):
-    def addTwoNumbers(self, l1, l2):
-        """
-        :type l1: Optional[ListNode]
-        :type l2: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
+    def addTwoNumbers(
+        self, l1: Optional[ListNode], l2: Optional[ListNode]
+    ) -> Optional[ListNode]:
         # Time O(n), Memory O(n) where n is max length of two linked lists
-        head = current = ListNode(0)
+        head = ListNode()
+        curr = head
         rem = 0
-
         while l1 or l2 or rem:
-            v = rem
-            if l1:
-                v += l1.val
-                l1 = l1.next
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
 
-            if l2:
-                v += l2.val
-                l2 = l2.next
+            val = v1 + v2 + rem
+            s = val % 10
+            rem = val // 10
 
-            if v >= 10:
-                current.next = ListNode(v - 10)
-                rem = 1
-            else:
-                current.next = ListNode(v)
-                rem = 0
+            curr.next = ListNode(s)
+            curr = curr.next
 
-            current = current.next
+            l1 = l1.next if l1 else l1
+            l2 = l2.next if l2 else l2
 
         return head.next
+
+    # def addTwoNumbers(self, l1, l2):
+    #     """
+    #     :type l1: Optional[ListNode]
+    #     :type l2: Optional[ListNode]
+    #     :rtype: Optional[ListNode]
+    #     """
+    #     # Time O(n), Memory O(n) where n is max length of two linked lists
+    #     head = current = ListNode(0)
+    #     rem = 0
+
+    #     while l1 or l2 or rem:
+    #         v = rem
+    #         if l1:
+    #             v += l1.val
+    #             l1 = l1.next
+
+    #         if l2:
+    #             v += l2.val
+    #             l2 = l2.next
+
+    #         if v >= 10:
+    #             current.next = ListNode(v - 10)
+    #             rem = 1
+    #         else:
+    #             current.next = ListNode(v)
+    #             rem = 0
+
+    #         current = current.next
+
+    #     return head.next
