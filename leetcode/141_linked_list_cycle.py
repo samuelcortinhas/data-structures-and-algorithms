ba8c76f179b8f9e9beba70ad1067cd34f34c4ce9
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-01-17 15:13:10 +0000

    improve setup

--- leetcode/141_linked_list_cycle.py
+++ leetcode/141_linked_list_cycle.py
@@ -1,27 +1,44 @@
+from typing import Optional
+
+
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 
 class Solution(object):
-    def hasCycle(self, head):
-        """
-        :type head: ListNode
-        :rtype: bool
-        """
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
         # Floyd's Tortoise and Hare
         # Time O(n), Memory O(1)
-        try:
-            slow = head
-            fast = head.next
-            while slow is not fast:
-                slow = slow.next
-                fast = fast.next.next
-            return True
-        except:
+        if not head:
             return False
+
+        slow, fast = head, head.next
+        while fast and fast.next:
+            if slow == fast:
+                return True
+            slow = slow.next
+            fast = fast.next.next
+        return False
+
+    # def hasCycleTryExcept(self, head):
+    #     """
+    #     :type head: ListNode
+    #     :rtype: bool
+    #     """
+    #     # Floyd's Tortoise and Hare
+    #     # Time O(n), Memory O(1)
+    #     try:
+    #         slow = head
+    #         fast = head.next
+    #         while slow is not fast:
+    #             slow = slow.next
+    #             fast = fast.next.next
+    #         return True
+    #     except:
+    #         return False
 
     # def hasCycleHistory(self, head):
     #     # Time O(n^2), Memory O(n)
