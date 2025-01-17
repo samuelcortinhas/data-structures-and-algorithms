47d825eb317f810f0e7d7227b7a8d07fab8a6dd7
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-01-17 15:57:44 +0000

    implement a stack

---
+++ core_skills/data_structures/stack.py
@@ -0,0 +1,27 @@
+class Stack(object):
+    """
+    Time O(1) for all methods
+    Memory O(n)
+    """
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val):
+        """
+        :type val: int
+        :rtype: None
+        """
+        self.stack.append(val)
+
+    def pop(self):
+        """
+        :rtype: None
+        """
+        self.stack.pop()
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1]
