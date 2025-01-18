417dc0d5033adf09ebfe6fc3e83656bbd00c9c95
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-01-18 21:20:15 +0000

    different variants of dfs

--- core_skills/algorithms/depth_first_search.py
+++ core_skills/algorithms/depth_first_search.py
@@ -5,18 +5,56 @@ class TreeNode:
         self.right = right
 
 
-def dfs(root):
-    # Time O(n), Memory O(h)
+# All are Time O(n), Memory O(h)
+def inorder(root):
     if not root:
         return
-    dfs(root.left)
+    inorder(root.left)
     print(root.val)
-    dfs(root.right)
+    inorder(root.right)
+
+
+def preorder(root):
+    if not root:
+        return
+    print(root.val)
+    preorder(root.left)
+    preorder(root.right)
+
+
+def postorder(root):
+    if not root:
+        return
+    postorder(root.left)
+    postorder(root.right)
+    print(root.val)
+
+
+def reverseorder(root):
+    if not root:
+        return
+    reverseorder(root.right)
+    print(root.val)
+    reverseorder(root.left)
 
 
 root = TreeNode(1)
 root.left = TreeNode(7)
 root.right = TreeNode(9)
 root.left.left = TreeNode(2)
+root.left.right = TreeNode(5)
+root.left.left.left = TreeNode(3)
 
-dfs(root)  # 2, 7, 1, 9
+#    1
+#   7  9
+#  2 5
+# 3
+
+print("in order")
+inorder(root)  # 3, 2, 7, 5, 1, 9
+print("\npre order")
+preorder(root)  # 1, 7, 2, 3, 5, 9
+print("\npost order")
+postorder(root)  # 3, 2, 5, 7, 9, 1
+print("\nreverse order")
+reverseorder(root)  # 9, 1, 5, 7, 2, 3
