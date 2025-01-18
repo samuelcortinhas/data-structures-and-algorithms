39c316348925a64a51d0b6ed9dca352c8968a08e
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-01-18 13:23:31 +0000

    example binary tree

--- core_skills/data_structures/binary_tree.py
+++ core_skills/data_structures/binary_tree.py
@@ -3,3 +3,9 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+
+
+root = TreeNode(1)
+root.left = TreeNode(7)
+root.right = TreeNode(9)
+root.left.left = TreeNode(2)
