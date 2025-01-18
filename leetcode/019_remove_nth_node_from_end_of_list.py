cd0a4611bb15ee7cffcc3eb6d48a677ea6cecffd
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-01-18 00:13:31 +0000

    single pass solution using dummy prefix list

--- leetcode/019_remove_nth_node_from_end_of_list.py
+++ leetcode/019_remove_nth_node_from_end_of_list.py
@@ -11,24 +11,53 @@ class ListNode:
 class Solution:
     # Time O(n), Memory O(1)
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        # get size of list
+        # create dummy list of length n
+        dummy = ListNode()
+        lag = dummy
+        for _ in range(n - 1):
+            dummy.next = ListNode()
+            dummy = dummy.next
+
+        # join dummy list to list
+        dummy.next = head
+
+        # iterate until end, then lag is (n+1)th from end
         curr = head
-        sz = 0
-        while curr:
+        sz = 1
+        while curr.next:
             sz += 1
             curr = curr.next
+            lag = lag.next
 
-        # base case
-        curr = head
-        if sz == n:
+        # remove n-th node from end
+        lag.next = lag.next.next
+
+        # edge case if removing head
+        if n == sz:
             return head.next
 
-        # go to previous node of node being removed
-        steps = sz - n - 1
-        for _ in range(steps):
-            curr = curr.next
-
-        # remove node
-        curr.next = curr.next.next
-
+        # return head
         return head
+
+    # def removeNthFromEndTwoPasses(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+    #     # get size of list
+    #     curr = head
+    #     sz = 0
+    #     while curr:
+    #         sz += 1
+    #         curr = curr.next
+
+    #     # base case
+    #     curr = head
+    #     if sz == n:
+    #         return head.next
+
+    #     # go to previous node of node being removed
+    #     steps = sz - n - 1
+    #     for _ in range(steps):
+    #         curr = curr.next
+
+    #     # remove node
+    #     curr.next = curr.next.next
+
+    #     return head
