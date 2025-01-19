773ecc4bfa4d5750c25dd68e2023ef6e619a220c
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-01-19 20:54:35 +0000

    direct search

--- leetcode/235_lowest_common_ancestor_of_a_binary_search_tree.py
+++ leetcode/235_lowest_common_ancestor_of_a_binary_search_tree.py
@@ -7,23 +7,38 @@ class TreeNode:
 
 
 class Solution:
-    def dfs(self, root, p_val, q_val):
-        # assume p_val < q_val
-        if not root:
-            return False
-
-        if root.val > q_val:
-            return self.dfs(root.left, p_val, q_val)
-        elif root.val < p_val:
-            return self.dfs(root.right, p_val, q_val)
-        else:
-            return root
-
     def lowestCommonAncestor(
         self, root: "TreeNode", p: "TreeNode", q: "TreeNode"
     ) -> "TreeNode":
-        # Time O(h), Memory O(h)
-        if p.val < q.val:
-            return self.dfs(root, p.val, q.val)
-        else:
-            return self.dfs(root, q.val, p.val)
+        # Time O(h), Memory O(1)
+        curr = root
+        while curr:
+            if curr.val > max(p.val, q.val):
+                curr = curr.left
+            elif curr.val < min(p.val, q.val):
+                curr = curr.right
+            else:
+                return curr
+
+
+# class Solution:
+#     def dfs(self, root, p_val, q_val):
+#         # assume p_val < q_val
+#         if not root:
+#             return False
+
+#         if root.val > q_val:
+#             return self.dfs(root.left, p_val, q_val)
+#         elif root.val < p_val:
+#             return self.dfs(root.right, p_val, q_val)
+#         else:
+#             return root
+
+#     def lowestCommonAncestor(
+#         self, root: "TreeNode", p: "TreeNode", q: "TreeNode"
+#     ) -> "TreeNode":
+#         # Time O(h), Memory O(h)
+#         if p.val < q.val:
+#             return self.dfs(root, p.val, q.val)
+#         else:
+#             return self.dfs(root, q.val, p.val)
