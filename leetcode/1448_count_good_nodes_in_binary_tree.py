4e816bc041a87ce07d118ae90a7ff8be5e2d92e1
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-01-20 13:58:16 +0000

    alt sol via recursive pre-order dfs

--- leetcode/1448_count_good_nodes_in_binary_tree.py
+++ leetcode/1448_count_good_nodes_in_binary_tree.py
@@ -9,31 +9,48 @@ class TreeNode:
 class Solution:
     # Time O(n), Memory O(h)
     def goodNodes(self, root: TreeNode) -> int:
-        # iterative dfs via 2 stacks
-        # node good if top of stack is max of stack
-        good_nodes = 0
-        stack_nodes = [root]
-        stack_vals = [root.val]
+        def dfs(root, mx):
+            # recursive pre-order dfs
+            if not root:
+                return 0
 
-        while stack_nodes:
-            node = stack_nodes[-1]
+            res = 1 if root.val >= mx else 0
+            mx = max(root.val, mx)
+            res += dfs(root.left, mx)
+            res += dfs(root.right, mx)
+            return res
 
-            if node.left:
-                stack_nodes.append(node.left)
-                stack_vals.append(node.left.val)
-                node.left = None
-                continue
+        return dfs(root, root.val)
 
-            if node.right:
-                stack_nodes.append(node.right)
-                stack_vals.append(node.right.val)
-                node.right = None
-                continue
 
-            if node.val == max(stack_vals):
-                good_nodes += 1
+# class Solution:
+#     # Time O(n), Memory O(h)
+#     def goodNodes(self, root: TreeNode) -> int:
+#         # iterative dfs via 2 stacks
+#         # node good if top of stack is max of stack
+#         good_nodes = 0
+#         stack_nodes = [root]
+#         stack_vals = [root.val]
 
-            stack_nodes.pop()
-            stack_vals.pop()
+#         while stack_nodes:
+#             node = stack_nodes[-1]
 
-        return good_nodes
+#             if node.left:
+#                 stack_nodes.append(node.left)
+#                 stack_vals.append(node.left.val)
+#                 node.left = None
+#                 continue
+
+#             if node.right:
+#                 stack_nodes.append(node.right)
+#                 stack_vals.append(node.right.val)
+#                 node.right = None
+#                 continue
+
+#             if node.val == max(stack_vals):
+#                 good_nodes += 1
+
+#             stack_nodes.pop()
+#             stack_vals.pop()
+
+#         return good_nodes
