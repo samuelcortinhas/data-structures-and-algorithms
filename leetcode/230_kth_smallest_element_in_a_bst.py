a186f7f8558cfb937b474c99aa37f1d4a34c2bb6
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-01-20 10:59:12 +0000

    first attempt

---
+++ leetcode/230_kth_smallest_element_in_a_bst.py
@@ -0,0 +1,31 @@
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def __init__(self):
+        self.counter = 0
+        self.res = None
+
+    def dfs(self, root, k):
+        # in order dfs
+        if not root:
+            return
+
+        self.dfs(root.left, k)
+        self.counter += 1
+        if self.counter == k:
+            self.res = root.val
+        self.dfs(root.right, k)
+
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        # Time O(n), Memory O(h)
+        self.dfs(root, k)
+        return self.res
