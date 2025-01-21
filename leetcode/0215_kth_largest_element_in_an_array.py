82068255b8391e07877f12b26a1887363764411c
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-01-21 15:21:29 +0000

    max heap

---
+++ leetcode/0215_kth_largest_element_in_an_array.py
@@ -0,0 +1,12 @@
+import heapq
+from typing import List
+
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        # Time O(k log n), Memory O(1)
+        nums = [-n for n in nums]
+        heapq.heapify(nums)
+        for _ in range(k - 1):
+            heapq.heappop(nums)
+        return -heapq.heappop(nums)
