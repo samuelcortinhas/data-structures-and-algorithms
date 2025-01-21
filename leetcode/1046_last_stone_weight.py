4b294b2c53ce5aeb057825f293168f334654d50d
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-01-21 13:17:44 +0000

    use max heap

---
+++ leetcode/1046_last_stone_weight.py
@@ -0,0 +1,20 @@
+import heapq
+from typing import List
+
+
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        # Time O(n log n), Memory O(1)
+        heap = [-s for s in stones]
+        heapq.heapify(heap)
+        while len(heap) > 1:
+            if len(heap) == 2:
+                return heap[1] - heap[0]
+
+            m1 = heapq.heappop(heap)
+            m2 = heapq.heappop(heap)
+
+            if m1 != m2:
+                heapq.heappush(heap, m1 - m2)
+
+        return -heap[0] if heap else 0
