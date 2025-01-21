81b5acc13c2fe554240690fda2efac058cecd6b1
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-01-21 14:34:23 +0000

    use min heap

--- leetcode/0973_k_closest_points_to_origin.py
+++ leetcode/0973_k_closest_points_to_origin.py
@@ -1,11 +1,21 @@
+import heapq
 from typing import List
 
 
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        # Time O(n log n), Memory O(n)
-        func = lambda x: (x[0] ** 2 + x[1] ** 2) ** 0.5
-        dists = [(func(x), x) for x in points]
-        sorted_dists = sorted(dists, key=lambda x: x[0])
-        topk = sorted_dists[:k]
-        return [x[1] for x in topk]
+        # Time O(k log n), Memory O(n)
+        heap = [(x[0] ** 2 + x[1] ** 2, x) for x in points]
+        heapq.heapify(heap)
+        res = []
+        for _ in range(k):
+            res.append(heapq.heappop(heap)[1])
+        return res
+
+    # def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+    #     # Time O(n log n), Memory O(n)
+    #     func = lambda x: (x[0] ** 2 + x[1] ** 2) ** 0.5
+    #     dists = [(func(x), x) for x in points]
+    #     sorted_dists = sorted(dists, key=lambda x: x[0])
+    #     topk = sorted_dists[:k]
+    #     return [x[1] for x in topk]
