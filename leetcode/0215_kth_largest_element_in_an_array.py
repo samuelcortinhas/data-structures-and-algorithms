6978854b59d34d1531144999ed797fd612dc0932
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-01-21 16:56:37 +0000

    max heap prefered

--- leetcode/0215_kth_largest_element_in_an_array.py
+++ leetcode/0215_kth_largest_element_in_an_array.py
@@ -3,27 +3,27 @@ from typing import List
 
 
 class Solution:
-    def findKthLargest(self, nums: List[int], k: int) -> int:
-        # Time O(n) on average, O(n^2) worst case. Memory O(1)
-        if k == 50000:
-            return 1  # work around
-        k = len(nums) - k
+    # def findKthLargest(self, nums: List[int], k: int) -> int:
+    #     # Time O(n) on average, O(n^2) worst case. Memory O(1)
+    #     if k == 50000:
+    #         return 1  # work around
+    #     k = len(nums) - k
 
-        def quickSelect(l, r):
-            p = l
-            for i in range(l, r + 1):
-                if nums[i] <= nums[r]:
-                    nums[i], nums[p] = nums[p], nums[i]
-                    p += 1
-            p -= 1
-            if p == k:
-                return nums[p]
-            elif p < k:
-                return quickSelect(p + 1, r)
-            else:
-                return quickSelect(l, p - 1)
+    #     def quickSelect(l, r):
+    #         p = l
+    #         for i in range(l, r + 1):
+    #             if nums[i] <= nums[r]:
+    #                 nums[i], nums[p] = nums[p], nums[i]
+    #                 p += 1
+    #         p -= 1
+    #         if p == k:
+    #             return nums[p]
+    #         elif p < k:
+    #             return quickSelect(p + 1, r)
+    #         else:
+    #             return quickSelect(l, p - 1)
 
-        return quickSelect(0, len(nums) - 1)
+    #     return quickSelect(0, len(nums) - 1)
 
     def findKthLargest(self, nums: List[int], k: int) -> int:
         # Time O(n + k*log n), Memory O(1)
