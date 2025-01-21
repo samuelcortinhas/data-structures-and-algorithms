11d359636f804cf79761c4221d9d9a440e469856
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-01-21 15:27:00 +0000

    fix time complexity

--- leetcode/0215_kth_largest_element_in_an_array.py
+++ leetcode/0215_kth_largest_element_in_an_array.py
@@ -4,7 +4,7 @@ from typing import List
 
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        # Time O(k log n), Memory O(1)
+        # Time O(n + k*log n), Memory O(1)
         nums = [-n for n in nums]
         heapq.heapify(nums)
         for _ in range(k - 1):
