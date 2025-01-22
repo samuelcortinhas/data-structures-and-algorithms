f1dbbb4ae4fba9fd458e8e989c45c97cef53f549
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-01-22 19:24:18 +0000

    backtracking is weird

---
+++ leetcode/0078_subset.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        # size of power set is 2^n
+        # every element is either in or not in a subset
+        # binary tree of height n
+        res = []
+        subset = []
+
+        def dfs(i):
+            # i = index of value we are making decision on
+            if i >= len(nums):
+                res.append(list(subset))
+                return
+
+            # decision to include nums[i]
+            subset.append(nums[i])
+            dfs(i + 1)
+
+            # decision to not include nums[i]
+            subset.pop()
+            dfs(i + 1)
+
+        dfs(0)
+        return res
