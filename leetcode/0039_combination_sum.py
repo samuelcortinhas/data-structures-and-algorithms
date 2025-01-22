84cb89f752df66bbf3605e53208244b897a0c64f
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-01-22 20:04:32 +0000

    speed up by keeping track of stack sum

--- leetcode/0039_combination_sum.py
+++ leetcode/0039_combination_sum.py
@@ -6,24 +6,48 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         # backtracking
         res = []
-        stack = []
 
-        def dfs(i):
+        def dfs(i, stack, stack_sum):
             # i = index of number being considered
-            s = sum(stack)
-            if s == target:
+            if stack_sum == target:
                 res.append(list(stack))
                 return
-            elif s > target or i >= len(candidates):
+            elif stack_sum > target or i >= len(candidates):
                 return
 
             # decision to include candidates[i] in stack
             stack.append(candidates[i])
-            dfs(i)
+            stack_sum += candidates[i]
+            dfs(i, stack, stack_sum)
 
             # decision not to include candidates[i] in stack
-            stack.pop()
-            dfs(i + 1)
+            stack_sum -= stack.pop()
+            dfs(i + 1, stack, stack_sum)
 
-        dfs(0)
+        dfs(0, [], 0)
         return res
+
+    # def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+    #     # backtracking
+    #     res = []
+    #     stack = []
+
+    #     def dfs(i):
+    #         # i = index of number being considered
+    #         s = sum(stack)
+    #         if s == target:
+    #             res.append(list(stack))
+    #             return
+    #         elif s > target or i >= len(candidates):
+    #             return
+
+    #         # decision to include candidates[i] in stack
+    #         stack.append(candidates[i])
+    #         dfs(i)
+
+    #         # decision not to include candidates[i] in stack
+    #         stack.pop()
+    #         dfs(i + 1)
+
+    #     dfs(0)
+    #     return res
