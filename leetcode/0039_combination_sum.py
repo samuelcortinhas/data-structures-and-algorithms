7c974c1913cf869e56c7bb2233b7efd2f34bafd2
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-01-22 19:46:08 +0000

    funky backtracking

---
+++ leetcode/0039_combination_sum.py
@@ -0,0 +1,28 @@
+from typing import List
+
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        # backtracking
+        res = []
+        stack = []
+
+        def dfs(i):
+            # i = index of number being considered
+            s = sum(stack)
+            if s == target:
+                res.append(list(stack))
+                return
+            elif s > target or i >= len(candidates):
+                return
+
+            # decision to include candidates[i] in stack
+            stack.append(candidates[i])
+            dfs(i)
+
+            # decision not to include candidates[i] in stack
+            stack.pop()
+            dfs(i + 1)
+
+        dfs(0)
+        return res
