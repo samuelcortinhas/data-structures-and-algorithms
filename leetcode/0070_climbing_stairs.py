a5eeb65fbb96605b014159dffeda6d7ff6154959
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-01-26 00:18:38 +0000

    optimal space complexity

--- leetcode/0070_climbing_stairs.py
+++ leetcode/0070_climbing_stairs.py
@@ -13,13 +13,31 @@ class Solution:
 
     #     return self.cache[n - 1] + self.cache[n - 2]
 
-    def climbStairs(self, n: int) -> int:
-        # Time O(n), Memory O(n)
-        cache = [0] * (n + 1)
-        cache[0] = 1
-        cache[1] = 1
+    # def climbStairsDP(self, n: int) -> int:
+    #     # Time O(n), Memory O(n)
+    #     cache = [0] * (n + 1)
+    #     cache[0] = 1
+    #     cache[1] = 1
 
-        for i in range(2, n + 1):
-            cache[i] = cache[i - 2] + cache[i - 1]
+    #     for i in range(2, n + 1):
+    #         cache[i] = cache[i - 2] + cache[i - 1]
 
-        return cache[-1]
+    #     return cache[-1]
+
+    # def climbStairsMemoryEfficient(self, n: int) -> int:
+    #     # Time O(n), Memory O(1)
+    #     cache = [1, 1]
+    #     for i in range(2, n + 1):
+    #         tmp = cache[1]
+    #         cache[1] = cache[0] + cache[1]
+    #         cache[0] = tmp
+    #     return cache[-1]
+
+    def climbStairsOptimal(self, n: int) -> int:
+        # Time O(n), Memory O(1)
+        prev, curr = 1, 1
+        for _ in range(2, n + 1):
+            tmp = curr
+            curr += prev
+            prev = tmp
+        return curr
