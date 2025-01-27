5a26454554cefd46e42406a89878579ec3a76b1f
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-01-27 21:27:25 +0000

    non stack solution

--- leetcode/0853_car_fleet.py
+++ leetcode/0853_car_fleet.py
@@ -11,3 +11,14 @@ class Solution:
             if t > stack[-1]:
                 stack.append(t)
         return len(stack) - 1
+
+    # def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+    #     # no stack, just use counter
+    #     max_t = 0
+    #     fleets = 0
+    #     for p, s in sorted(zip(position, speed))[::-1]:
+    #         t = (target - p) / s
+    #         if t > max_t:
+    #             fleets += 1
+    #             max_t = t
+    #     return fleets
