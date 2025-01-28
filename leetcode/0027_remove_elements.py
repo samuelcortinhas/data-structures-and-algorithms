1271eb4d9db946d17c37ab012c2b6ce902c74116
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-01-28 22:31:49 +0000

    linear solution

--- leetcode/0027_remove_elements.py
+++ leetcode/0027_remove_elements.py
@@ -3,10 +3,19 @@ from typing import List
 
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        # Time O(n^2), Memory O(1)
-        deletes = 0
+        # Time O(n), Memory O(1)
+        index = 0
         for i in range(len(nums)):
-            if nums[i - deletes] == val:
-                nums.remove(val)
-                deletes += 1
-        return len(nums)
+            if nums[i] != val:
+                nums[index] = nums[i]
+                index += 1
+        return index
+
+    # def removeElement(self, nums: List[int], val: int) -> int:
+    #     # Time O(n^2), Memory O(1)
+    #     deletes = 0
+    #     for i in range(len(nums)):
+    #         if nums[i - deletes] == val:
+    #             nums.remove(val)
+    #             deletes += 1
+    #     return len(nums)
