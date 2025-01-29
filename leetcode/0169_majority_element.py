e319f8cc10f769e870f514922ed2842d7e9c49be
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-01-29 22:51:01 +0000

    boyer moore majority vote algorithm

--- leetcode/0169_majority_element.py
+++ leetcode/0169_majority_element.py
@@ -1,10 +1,20 @@
-from collections import Counter
+# from collections import Counter
 from typing import List
 
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        # Time O(n log n), Memory O(n)
-        counter = Counter(nums)
-        s = sorted(counter.items(), key=lambda x: x[1], reverse=True)
-        return s[0][0]
+        # Boyer–Moore majority vote algorithm
+        # Time O(n), Memory O(1)
+        maj_num, count = 0, 0
+        for n in nums:
+            if count == 0:
+                maj_num = n
+            count += 1 if n == maj_num else -1
+        return maj_num
+
+    # def majorityElementSort(self, nums: List[int]) -> int:
+    #     # Time O(n log n), Memory O(n)
+    #     counter = Counter(nums)
+    #     s = sorted(counter.items(), key=lambda x: x[1], reverse=True)
+    #     return s[0][0]
