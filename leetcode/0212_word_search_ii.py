5afd946d18d79cbf374d614ec35e5286dbb7e856
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-01-30 22:10:33 +0000

    every trie node defines a trie so add add_word method to node class

--- leetcode/0212_word_search_ii.py
+++ leetcode/0212_word_search_ii.py
@@ -4,35 +4,27 @@ from typing import List
 # Idea: store words in a prefix tree. DFS on grid.
 class TrieNode:
     def __init__(self):
-        self.children = {}
-        self.word = False
-
-
-class Trie:
-    def __init__(self):
         # Memory O(N*W) where N=#words, W=max word len
-        self.root = TrieNode()
+        self.children = {}
+        self.is_word = False
 
-    def _add_word(self, word):
+    def add_word(self, word):
         # Time O(W)
-        curr = self.root
+        curr = self
         for c in word:
             if c not in curr.children:
                 curr.children[c] = TrieNode()
             curr = curr.children[c]
-        curr.word = True
-
-    def __call__(self, words):
-        for word in words:
-            self._add_word(word)
-        return self.root
+        curr.is_word = True
 
 
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
         # Time O(m*n*4^(m*n) + N*W), Memory O(m*n + N*W)
         m, n = len(board), len(board[0])
-        root = Trie()(words)
+        root = TrieNode()
+        for word in words:
+            root.add_word(word)
         res, seen = set(), set()
 
         def dfs(i, j, node, curr):
