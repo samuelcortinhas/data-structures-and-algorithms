56e3c4fa2010d5b239453dc69090410948f70aa6
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-02-01 10:43:53 +0000

    floyds slow and fast algorithm - just memorise this

--- leetcode/0287_find_the_duplicate_number.py
+++ leetcode/0287_find_the_duplicate_number.py
@@ -3,9 +3,23 @@ from typing import List
 
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        # Time O(n), Memory O(n)
-        seen = set()
-        for n in nums:
-            if n in seen:
-                return n
-            seen.add(n)
+        # Time O(n), Memory O(1)
+        # Floyd's cycle detection
+        slow, fast = nums[0], nums[nums[0]]
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+
+        slow2 = 0
+        while slow != slow2:
+            slow = nums[slow]
+            slow2 = nums[slow2]
+        return slow
+
+    # def findDuplicateWithSet(self, nums: List[int]) -> int:
+    #     # Time O(n), Memory O(n)
+    #     seen = set()
+    #     for n in nums:
+    #         if n in seen:
+    #             return n
+    #         seen.add(n)
