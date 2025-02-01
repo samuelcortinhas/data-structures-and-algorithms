1a38b22abf634b7350a54786e2ed0caedb6ca9b0
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-02-01 11:02:15 +0000

    add explanation for why second slow2 pointer works

--- leetcode/0287_find_the_duplicate_number.py
+++ leetcode/0287_find_the_duplicate_number.py
@@ -1,5 +1,29 @@
 from typing import List
 
+"""
+Explanation. 
+nums gives rise to a linked list with a cycle starting at the duplicate number.
+We use Floyd's slow and fast algorithm to identify the cycle. 
+But where slow=fast isn't actually the duplicate number.
+
+Let p be the number of steps from 0 to duplicate number. (i.e. #previous steps)
+Let x be the number of steps from where slow=fast to the duplicate number.
+Let c by the length of the cycle. 
+
+The slow pointer travels p + c-x steps. 
+The fast pointer travels p + kc + c-x, where k is an unknown integer. 
+We also know that fast = 2*slow, therefore we have
+
+2(p+c-x) = p+kc+c-x
+2p+2c-2x = p+kc+c-x
+p+c = kc+x
+p = x + (k-1)c
+
+And since the first slow pointer is inside the cycle, taking mod c we get p=x.
+That means we can use a second slow2 pointer and iterate until slow=slow2,
+ensuring that we arrive at the duplicate number.
+"""
+
 
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
