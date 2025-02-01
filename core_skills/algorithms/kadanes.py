b029785c6becb0b48f2343187818c64a0c7582ba
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-02-01 14:26:27 +0000

    sliding window approach

--- core_skills/algorithms/kadanes.py
+++ core_skills/algorithms/kadanes.py
@@ -9,5 +9,26 @@ def kadanes(arr):
     return max_sum
 
 
+def kadanes_sliding_window(arr):
+    # Time O(n), Memory O(1)
+    # Returns indexes of subarray boundaries
+    curr_sum = 0
+    max_sum = float("-inf")
+    i, j = 0, 0
+    res_i, res_j = 0, 0
+
+    while j < len(arr):
+        if curr_sum < 0:
+            curr_sum = 0
+            i = j
+        curr_sum += arr[j]
+        if curr_sum > max_sum:
+            max_sum = curr_sum
+            res_i, res_j = i, j
+        j += 1
+    return res_i, res_j
+
+
 arr = [4, -1, 2, -7, 3, 4]
 print(kadanes(arr))
+print(kadanes_sliding_window(arr))
