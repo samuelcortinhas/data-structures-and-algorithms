562d4a8962d787e0b003ae39506808e628e0bf17
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-02-01 14:11:45 +0000

    implement kadanes algorithm

---
+++ core_skills/algorithms/kadanes.py
@@ -0,0 +1,12 @@
+def kadanes(arr):
+    curr_sum = 0
+    max_sum = 0
+    for n in arr:
+        curr_sum = max(curr_sum, 0)
+        curr_sum += n
+        max_sum = max(max_sum, curr_sum)
+    return max_sum
+
+
+arr = [4, -1, 2, -7, 3, 4]
+print(kadanes(arr))
