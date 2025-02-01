41ee5678be89bfe8e817c872be066000e21a1f1a
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-02-01 15:10:33 +0000

    faster solution using set

--- leetcode/0219_contains_duplicate_ii.py
+++ leetcode/0219_contains_duplicate_ii.py
@@ -3,9 +3,25 @@ from typing import List
 
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        # Time O(n*k), Memory O(1)
-        for left in range(len(nums)):
-            for right in range(left + 1, min(left + k + 1, len(nums))):
-                if nums[left] == nums[right]:
-                    return True
+        # Time O(n), Memory O(n)
+        seen = set(nums[:k])
+        if len(seen) != len(nums[:k]):
+            return True
+
+        left, right = 0, k
+        while right < len(nums):
+            if nums[right] in seen:
+                return True
+            seen.add(nums[right])
+            seen.remove(nums[left])
+            left += 1
+            right += 1
         return False
+
+    # def containsNearbyDuplicateSlow(self, nums: List[int], k: int) -> bool:
+    #     # Time O(n*k), Memory O(1)
+    #     for left in range(len(nums)):
+    #         for right in range(left + 1, min(left + k + 1, len(nums))):
+    #             if nums[left] == nums[right]:
+    #                 return True
+    #     return False
