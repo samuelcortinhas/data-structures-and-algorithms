b493397b551e9cd456419b44c5f7a8c9bf87427a
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-02-02 16:14:23 +0000

    prefix sum complexities

--- leetcode/0303_range_sum_query-immutable.py
+++ leetcode/0303_range_sum_query-immutable.py
@@ -3,6 +3,7 @@ from typing import List
 
 class NumArray:
     def __init__(self, nums: List[int]):
+        # Time O(n), Memory O(n)
         self.nums = nums
         self.prefix_sum = []
         total = 0
@@ -11,6 +12,7 @@ class NumArray:
             self.prefix_sum.append(total)
 
     def sumRange(self, left: int, right: int) -> int:
+        # Time O(1), Memory O(1)
         r = self.prefix_sum[right]
         l = self.prefix_sum[left - 1] if left > 0 else 0
         return r - l
