3c0bda47257ec23a1951606b1c880e08f0331fd9
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-02-02 10:30:10 +0000

    2 pointer solution

--- leetcode/0080_remove_duplicates_from_sorted_array_ii.py
+++ leetcode/0080_remove_duplicates_from_sorted_array_ii.py
@@ -4,12 +4,26 @@ from typing import List
 
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        # Time O(n), Memory O(n)
-        counter = defaultdict(int)
-        i = 0
-        for n in nums:
-            if counter[n] <= 1:
-                nums[i] = n
+        # Time O(n), Memory O(1)
+        i = 1  # insert index
+        count = 1
+        for j in range(1, len(nums)):
+            if nums[j] == nums[j - 1]:
+                count += 1
+            else:
+                count = 1
+            if count <= 2:
+                nums[i] = nums[j]
                 i += 1
-                counter[n] += 1
         return i
+
+    # def removeDuplicatesCounter(self, nums: List[int]) -> int:
+    #     # Time O(n), Memory O(n)
+    #     counter = defaultdict(int)
+    #     i = 0
+    #     for n in nums:
+    #         if counter[n] <= 1:
+    #             nums[i] = n
+    #             i += 1
+    #             counter[n] += 1
+    #     return i
