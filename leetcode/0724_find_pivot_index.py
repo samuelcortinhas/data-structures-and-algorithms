a5ac238e030939f0cb0a47119dcb80fb2013f9e7
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-02-02 17:42:57 +0000

    memory efficient solution, increment prefix sum and deincrement postfix sum

--- leetcode/0724_find_pivot_index.py
+++ leetcode/0724_find_pivot_index.py
@@ -3,21 +3,31 @@ from typing import List
 
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
-        # Time O(n), Memory O(n)
-        prefix = []
-        total = 0
-        for n in nums:
-            prefix.append(total)
-            total += n
-
-        postfix = []
-        total = 0
-        for n in nums[::-1]:
-            postfix.append(total)
-            total += n
-        postfix = postfix[::-1]
-
-        for i, (l, r) in enumerate(zip(prefix, postfix)):
-            if l == r:
+        # Time O(n), Memory O(1)
+        leftsum, rightsum = 0, sum(nums)
+        for i, n in enumerate(nums):
+            rightsum -= n
+            if leftsum == rightsum:
                 return i
+            leftsum += n
         return -1
+
+    # def pivotIndex3Loops(self, nums: List[int]) -> int:
+    #     # Time O(n), Memory O(n)
+    #     prefix = []
+    #     total = 0
+    #     for n in nums:
+    #         prefix.append(total)
+    #         total += n
+
+    #     postfix = []
+    #     total = 0
+    #     for n in nums[::-1]:
+    #         postfix.append(total)
+    #         total += n
+    #     postfix = postfix[::-1]
+
+    #     for i, (l, r) in enumerate(zip(prefix, postfix)):
+    #         if l == r:
+    #             return i
+    #     return -1
