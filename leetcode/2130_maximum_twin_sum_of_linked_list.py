bf6e1db62712c3a86b617798f2abbad27697e4d1
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-02-03 12:54:47 +0000

    fast and slow pointers

---
+++ leetcode/2130_maximum_twin_sum_of_linked_list.py
@@ -0,0 +1,29 @@
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def pairSum(self, head: Optional[ListNode]) -> int:
+        # Time O(n), Memory O(n)
+        prev = None
+        slow, fast = head, head
+        while fast and fast.next:
+            slow.prev = prev
+            prev = slow
+            slow = slow.next
+            fast = fast.next.next
+
+        right = slow
+        left = prev
+        res = 0
+        while right:
+            res = max(res, right.val + left.val)
+            right = right.next
+            left = left.prev
+        return res
