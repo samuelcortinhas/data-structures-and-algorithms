5f0ba10cf4813ebbd1056b5891bbf0f492d5415c
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-02-05 00:03:25 +0000

    reuse previous work for previous power of 2

--- leetcode/0338_counting_bits.py
+++ leetcode/0338_counting_bits.py
@@ -1,14 +1,25 @@
+import math
 from typing import List
 
 
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        # Time O(n log n), Memory O(n)
-        def countOnes(n):
-            count = 0
-            while n:
-                count += n & 1
-                n = n >> 1
-            return count
+        # Time O(n), Memory O(n)
+        lg2 = math.ceil(math.log2(1 + n))
+        res = [0] * (n + 1)
+        for i in range(lg2):
+            k = 2**i
+            res[k : 2 * k] = [r + 1 for r in res[:k]]
+        return res[: n + 1]
 
-        return [countOnes(i) for i in range(n + 1)]
+
+#     def countBitsBruteForce(self, n: int) -> List[int]:
+#         # Time O(n log n), Memory O(n)
+#         def countOnes(n):
+#             count = 0
+#             while n:
+#                 count += n & 1
+#                 n = n >> 1
+#             return count
+
+#         return [countOnes(i) for i in range(n + 1)]
