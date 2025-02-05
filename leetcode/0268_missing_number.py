4ab4fdcdb65c7ef414d72bdf33a1cfb28874dbc0
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-02-05 09:15:59 +0000

    simplify

--- leetcode/0268_missing_number.py
+++ leetcode/0268_missing_number.py
@@ -4,4 +4,4 @@ from typing import List
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         # Time O(1), Memory O(1)
-        return int(len(nums) * (len(nums) + 1) / 2 - sum(nums))
+        return len(nums)*(len(nums)+1)//2 - sum(nums)
