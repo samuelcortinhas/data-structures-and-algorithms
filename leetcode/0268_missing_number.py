8f09eed3d15c23838977fb7ef0bcea4cfc58e98c
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-02-05 19:38:59 +0000

    xor with natural numbers

--- leetcode/0268_missing_number.py
+++ leetcode/0268_missing_number.py
@@ -3,5 +3,12 @@ from typing import List
 
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        # Time O(1), Memory O(1)
-        return len(nums)*(len(nums)+1)//2 - sum(nums)
+        # Time O(n), Memory O(1)
+        res = 0
+        for i, n in enumerate(nums):
+            res ^= i ^ n
+        return res ^ len(nums)
+
+    # def missingNumberMaths(self, nums: List[int]) -> int:
+    #     # Time O(1), Memory O(1)
+    #     return len(nums)*(len(nums)+1)//2 - sum(nums)
