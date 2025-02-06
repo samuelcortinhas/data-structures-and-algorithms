50430a002e8a8303aba18d79670d93bc4308e77f
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-02-06 21:47:29 +0000

    slicing

--- leetcode/0028_find_the_index_of_the_first_occurence_in_a_string.py
+++ leetcode/0028_find_the_index_of_the_first_occurence_in_a_string.py
@@ -1,3 +1,10 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        return haystack.find(needle)
+        # Time O(n*m), Memory O(1) where n = len(haystack), m = len(needle)
+        for i in range(len(haystack) - len(needle) + 1):
+            if haystack[i : i + len(needle)] == needle:
+                return i
+        return -1
+
+    # def strStrFind(self, haystack: str, needle: str) -> int:
+    #     return haystack.find(needle)
