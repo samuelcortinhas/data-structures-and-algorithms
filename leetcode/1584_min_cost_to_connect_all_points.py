6b79efed23867796bb371286b058bd03a414f484
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-02-08 11:12:50 +0000

    alt solution using kruskals

--- leetcode/1584_min_cost_to_connect_all_points.py
+++ leetcode/1584_min_cost_to_connect_all_points.py
@@ -4,6 +4,7 @@ from typing import List
 
 
 class Solution:
+    # Prim's Algorithm
     @staticmethod
     def manhattan(p1, p2):
         return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
@@ -39,3 +40,60 @@ class Solution:
             for neigh in adj_list[dest]:
                 if neigh not in seen:
                     heapq.heappush(heap, (self.manhattan(dest, neigh), dest, neigh))
+
+
+class Solution:
+    # Kruskal's algorithm
+    @staticmethod
+    def manhattan(p1, p2):
+        return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
+
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        # Time O(n^2 log n), Memory O(n^2)
+        n = len(points)
+        if n <= 1:
+            return 0
+        edges = []
+        for i in range(len(points)):
+            for j in range(i + 1, len(points)):
+                p, q = tuple(points[i]), tuple(points[j])
+                edges.append((p, q, self.manhattan(p, q)))
+
+        sorted_edges = sorted(edges, key=lambda x: x[2])
+        points_map = {tuple(p): i for i, p in enumerate(points)}
+        uf = UnionFind(n)
+        cost = 0
+        mst_size = 0
+        for u, v, w in sorted_edges:
+            if uf.union(points_map[u], points_map[v]):
+                cost += w
+                mst_size += 1
+                if mst_size == n - 1:
+                    return cost
+
+
+class UnionFind:
+    def __init__(self, n_nodes):
+        self.par = [i for i in range(n_nodes)]
+        self.height = [0 for _ in range(n_nodes)]
+
+    def find(self, p):
+        while p != self.par[p]:
+            self.par[p] = self.par[self.par[p]]  # path compression
+            p = self.par[p]
+        return p
+
+    def union(self, node1, node2):
+        p1, p2 = self.find(node1), self.find(node2)
+        if p1 == p2:
+            return False
+
+        # union by height
+        if self.height[p1] > self.height[p2]:
+            self.par[p2] = p1
+        elif self.height[p1] < self.height[p2]:
+            self.par[p1] = p2
+        else:
+            self.par[p2] = p1
+            self.height[p1] += 1
+        return True
