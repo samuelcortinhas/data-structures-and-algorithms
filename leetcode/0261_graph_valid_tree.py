493112a9c1d1a1d4e2dc2e273dafa8f90606b6d1
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-02-09 12:36:17 +0000

    alternative solution using dfs and visit set

--- leetcode/0261_graph_valid_tree.py
+++ leetcode/0261_graph_valid_tree.py
@@ -1,48 +1,76 @@
+from collections import defaultdict
 from typing import List
 
 
 class Solution:
-    # Detect cycles and if graph is connected using UnionFind
+    # Detect cycles and if graph is connected using DFS
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        # Time O(V+E), Memory O(V)
-        uf = UnionFind(n)
-
+        # Time O(V+E), Memory O(E)
+        adj_list = defaultdict(list)
         for a, b in edges:
-            if not uf.union(a, b):
+            adj_list[a].append(b)
+            adj_list[b].append(a)
+
+        visit = set()
+
+        def dfs(node, parent):
+            if node in visit:
                 return False
 
-        par = uf.find(0)
-        for i in range(1, n):
-            if uf.find(i) != par:
-                return False
-        return True
+            visit.add(node)
+            for neigh in adj_list[node]:
+                if neigh != parent:
+                    if not dfs(neigh, node):
+                        return False
+            return True
 
-
-class UnionFind:
-    def __init__(self, n_nodes):
-        # Memory O(V)
-        self.par = [i for i in range(n_nodes)]
-        self.height = [0] * n_nodes
-
-    def find(self, p):
-        # Time O(a(n)) where a(.) is inverse ackermann function
-        while p != self.par[p]:
-            self.par[p] = self.par[self.par[p]]  # path compression
-            p = self.par[p]
-        return p
-
-    def union(self, node1, node2):
-        # Time O(a(n)) where a(.) is inverse ackermann function
-        p1, p2 = self.find(node1), self.find(node2)
-        if p1 == p2:
+        if not dfs(0, None):
             return False
+        return len(visit) == n
 
-        # union by height
-        if self.height[p1] > self.height[p2]:
-            self.par[p2] = p1
-        elif self.height[p1] < self.height[p2]:
-            self.par[p1] = p2
-        else:
-            self.par[p2] = p1
-            self.height[p1] += 1
-        return True
+
+# class Solution:
+#     # Detect cycles and if graph is connected using UnionFind
+#     def validTree(self, n: int, edges: List[List[int]]) -> bool:
+#         # Time O(V+E), Memory O(V)
+#         uf = UnionFind(n)
+
+#         for a, b in edges:
+#             if not uf.union(a, b):
+#                 return False
+
+#         par = uf.find(0)
+#         for i in range(1, n):
+#             if uf.find(i) != par:
+#                 return False
+#         return True
+
+
+# class UnionFind:
+#     def __init__(self, n_nodes):
+#         # Memory O(V)
+#         self.par = [i for i in range(n_nodes)]
+#         self.height = [0] * n_nodes
+
+#     def find(self, p):
+#         # Time O(a(n)) where a(.) is inverse ackermann function
+#         while p != self.par[p]:
+#             self.par[p] = self.par[self.par[p]]  # path compression
+#             p = self.par[p]
+#         return p
+
+#     def union(self, node1, node2):
+#         # Time O(a(n)) where a(.) is inverse ackermann function
+#         p1, p2 = self.find(node1), self.find(node2)
+#         if p1 == p2:
+#             return False
+
+#         # union by height
+#         if self.height[p1] > self.height[p2]:
+#             self.par[p2] = p1
+#         elif self.height[p1] < self.height[p2]:
+#             self.par[p1] = p2
+#         else:
+#             self.par[p2] = p1
+#             self.height[p1] += 1
+#         return True
