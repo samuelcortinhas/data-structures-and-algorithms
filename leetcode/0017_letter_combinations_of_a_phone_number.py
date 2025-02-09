ea5650380d7694e48a6cdcc439f7421c7a659267
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-02-09 21:56:57 +0000

    can iterate over string directly

--- leetcode/0017_letter_combinations_of_a_phone_number.py
+++ leetcode/0017_letter_combinations_of_a_phone_number.py
@@ -5,14 +5,14 @@ class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         # Time O(4^n), Memory O(4^n)
         phone_map = {
-            "2": ["a", "b", "c"],
-            "3": ["d", "e", "f"],
-            "4": ["g", "h", "i"],
-            "5": ["j", "k", "l"],
-            "6": ["m", "n", "o"],
-            "7": ["p", "q", "r", "s"],
-            "8": ["t", "u", "v"],
-            "9": ["w", "x", "y", "z"],
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz",
         }
         nums = list(digits)
         res = []
