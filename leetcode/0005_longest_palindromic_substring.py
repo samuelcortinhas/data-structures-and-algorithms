ae06ea153e8e9fc4986a80612250cca73450b06b
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-02-10 18:38:31 +0000

    combine even and odd palindromes using offset

--- leetcode/0005_longest_palindromic_substring.py
+++ leetcode/0005_longest_palindromic_substring.py
@@ -3,20 +3,11 @@ class Solution:
         # Time O(n^2), Memory O(1)
         res = ""
         for i in range(len(s)):
-            # odd palindromes
-            l, r = i, i
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                if r - l + 1 > len(res):
-                    res = s[l : r + 1]
-                l -= 1
-                r += 1
-
-            # even palindromes
-            l, r = i, i + 1
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                if r - l + 1 > len(res):
-                    res = s[l : r + 1]
-                l -= 1
-                r += 1
-
+            for offset in [0, 1]:  # odd/even palindromes
+                l, r = i, i + offset
+                while l >= 0 and r < len(s) and s[l] == s[r]:
+                    if r - l + 1 > len(res):
+                        res = s[l : r + 1]
+                    l -= 1
+                    r += 1
         return res
