f0d5c395a28ac035ce789c1a0e5aeac0f455bd2e
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-02-11 18:00:17 +0000

    elegant solution

--- leetcode/0066_plus_one.py
+++ leetcode/0066_plus_one.py
@@ -4,23 +4,35 @@ from typing import List
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         # Time O(n), O(1)
-        if digits[-1] < 9:
-            digits[-1] += 1
-            return digits
-
-        digits = digits[::-1]
-        rem = 1
-        for i in range(len(digits)):
-            d = digits[i]
-            if d == 9:
+        i = len(digits) - 1
+        while i >= 0:
+            if digits[i] == 9:
                 digits[i] = 0
             else:
                 digits[i] += 1
-                rem = 0
-                break
-        if rem:
-            digits.append(1)
-        return digits[::-1]
+                return digits
+            i -= 1
+        return [1] + digits
+
+    # def plusOne(self, digits: List[int]) -> List[int]:
+    #     # Time O(n), O(1)
+    #     if digits[-1] < 9:
+    #         digits[-1] += 1
+    #         return digits
+
+    #     digits = digits[::-1]
+    #     rem = 1
+    #     for i in range(len(digits)):
+    #         d = digits[i]
+    #         if d == 9:
+    #             digits[i] = 0
+    #         else:
+    #             digits[i] += 1
+    #             rem = 0
+    #             break
+    #     if rem:
+    #         digits.append(1)
+    #     return digits[::-1]
 
     # def plusOne(self, digits: List[int]) -> List[int]:
     #     # Time O(n), O(n)
