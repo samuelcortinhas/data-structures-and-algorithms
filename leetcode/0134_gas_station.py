6cc6396a01e08e4ca09f2d17f72f62cd31261c65
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-02-14 13:45:38 +0000

    compute diffs of gas and cost for v2

--- leetcode/0134_gas_station.py
+++ leetcode/0134_gas_station.py
@@ -2,20 +2,35 @@ from typing import List
 
 
 class Solution:
-    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        # Time O(n^2), Memory O(1)
+    def canCompleteCircuitV2(self, gas: List[int], cost: List[int]) -> int:
+        # Time O(n^2), Memory O(n)
         n = len(gas)
+        diff = [g - c for g, c in zip(gas, cost)]
+        print(diff)
         for i in range(n):
-            tank = gas[i]
-            if not tank:
-                continue
-            stop = False
+            tank = 0
             for j in range(n):
-                tank -= cost[(i + j) % n]
+                tank += diff[(i + j) % n]
                 if tank < 0:
-                    stop = True
                     break
-                tank += gas[(i + j + 1) % n]
-            if not stop:
+            if tank >= 0:
                 return i
         return -1
+
+    # def canCompleteCircuitV1(self, gas: List[int], cost: List[int]) -> int:
+    #     # Time O(n^2), Memory O(1)
+    #     n = len(gas)
+    #     for i in range(n):
+    #         tank = gas[i]
+    #         if not tank:
+    #             continue
+    #         stop = False
+    #         for j in range(n):
+    #             tank -= cost[(i + j) % n]
+    #             if tank < 0:
+    #                 stop = True
+    #                 break
+    #             tank += gas[(i + j + 1) % n]
+    #         if not stop:
+    #             return i
+    #     return -1
