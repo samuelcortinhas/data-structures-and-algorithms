a8a768c65069e5a194282cfacbc93762ea0306fb
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-02-14 18:55:03 +0000

    one line solution

--- leetcode/3110_score_of_a_string.py
+++ leetcode/3110_score_of_a_string.py
@@ -5,3 +5,7 @@ class Solution:
         for i in range(len(s) - 1):
             res += abs(ord(s[i + 1]) - ord(s[i]))
         return res
+
+    # def scoreOfStringOneLiner(self, s: str) -> int:
+    #     # Time O(n), Memory O(1)
+    #     return sum([abs(ord(s[i+1])-ord(s[i])) for i in range(len(s)-1)])
