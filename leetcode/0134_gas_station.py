3c873b80a3db9391f9f0f5953448f74738620dca
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-02-14 13:53:43 +0000

    sliding window

--- leetcode/0134_gas_station.py
+++ leetcode/0134_gas_station.py
@@ -2,20 +2,37 @@ from typing import List
 
 
 class Solution:
-    def canCompleteCircuitV2(self, gas: List[int], cost: List[int]) -> int:
-        # Time O(n^2), Memory O(n)
+    def canCompleteCircuitV3(self, gas: List[int], cost: List[int]) -> int:
+        # Time O(n), Memory O(n)
         n = len(gas)
-        diff = [g - c for g, c in zip(gas, cost)]
-        print(diff)
-        for i in range(n):
-            tank = 0
-            for j in range(n):
-                tank += diff[(i + j) % n]
-                if tank < 0:
-                    break
-            if tank >= 0:
-                return i
+        dp = [g - c for g, c in zip(gas, cost)]
+        tank = 0
+        window_length = 0
+        for i in range(2 * n):
+            tank += dp[i % n]
+            window_length += 1
+            if tank >= 0 and window_length == n:
+                return (i + 1) % n
+            if tank < 0:
+                tank = 0
+                window_length = 0
+                continue
         return -1
+
+    # def canCompleteCircuitV2(self, gas: List[int], cost: List[int]) -> int:
+    #     # Time O(n^2), Memory O(n)
+    #     n = len(gas)
+    #     diff = [g - c for g, c in zip(gas, cost)]
+    #     print(diff)
+    #     for i in range(n):
+    #         tank = 0
+    #         for j in range(n):
+    #             tank += diff[(i + j) % n]
+    #             if tank < 0:
+    #                 break
+    #         if tank >= 0:
+    #             return i
+    #     return -1
 
     # def canCompleteCircuitV1(self, gas: List[int], cost: List[int]) -> int:
     #     # Time O(n^2), Memory O(1)
