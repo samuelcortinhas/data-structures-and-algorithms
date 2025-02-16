b177f43f1ca5bcf78d7d30b635dc8cf733d716a4
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-02-16 10:00:20 +0000

    remove unnecessaary continue

--- leetcode/0435_non-overlapping_intervals.py
+++ leetcode/0435_non-overlapping_intervals.py
@@ -10,7 +10,6 @@ class Solution:
         for i in range(1, len(intervals)):
             if curr[1] <= intervals[i][0]:
                 curr = intervals[i]
-                continue
             elif curr[1] <= intervals[i][1]:
                 deletes += 1
             else:
