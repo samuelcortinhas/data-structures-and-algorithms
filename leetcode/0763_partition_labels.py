b1f83d41320e640262a36d53c5e308e0f242350c
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-02-16 17:08:19 +0000

    linear solution with comparing interval end with current index

--- leetcode/0763_partition_labels.py
+++ leetcode/0763_partition_labels.py
@@ -2,28 +2,45 @@ from typing import List
 
 
 class Solution:
-    def partitionLabels(self, s: str) -> List[int]:
-        # Time O(n log n), Memory O(n)
-        start = {}
-        end = {}
+    def partitionLabelsV2(self, s: str) -> List[int]:
+        # Time O(n), Memory O(26)
+        last_occurence = {}
         for i, char in enumerate(s):
-            if char not in start:
-                start[char] = i
-            end[char] = i
+            last_occurence[char] = i
 
-        intervals = []
-        for k, v in start.items():
-            intervals.append([v, end[k]])
-        intervals.sort()
-
-        # merge intervals
         res = []
-        curr = intervals[0]
-        for i in range(1, len(intervals)):
-            if curr[1] < intervals[i][0]:
-                res.append(curr)
-                curr = intervals[i]
-            else:
-                curr[1] = max(curr[1], intervals[i][1])
-        res.append(curr)
-        return [e - s + 1 for s, e in res]
+        size, max_size, end = 0, 0, 0
+        for i, char in enumerate(s):
+            size += 1
+            max_size = max(max_size, size)
+            end = max(end, last_occurence[char])
+            if i >= end:
+                res.append(size)
+                size = 0
+        return res
+
+    # def partitionLabelsV1(self, s: str) -> List[int]:
+    #     # Time O(n log n), Memory O(n)
+    #     start = {}
+    #     end = {}
+    #     for i, char in enumerate(s):
+    #         if char not in start:
+    #             start[char] = i
+    #         end[char] = i
+
+    #     intervals = []
+    #     for k, v in start.items():
+    #         intervals.append([v, end[k]])
+    #     intervals.sort()
+
+    #     # merge intervals
+    #     res = []
+    #     curr = intervals[0]
+    #     for i in range(1, len(intervals)):
+    #         if curr[1] < intervals[i][0]:
+    #             res.append(curr)
+    #             curr = intervals[i]
+    #         else:
+    #             curr[1] = max(curr[1], intervals[i][1])
+    #     res.append(curr)
+    #     return [e - s + 1 for s, e in res]
