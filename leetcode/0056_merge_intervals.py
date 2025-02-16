96f59965e794af81ab5cf0c47cc1746c10de9ca6
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-02-16 00:27:21 +0000

    sort intervals in place

--- leetcode/0056_merge_intervals.py
+++ leetcode/0056_merge_intervals.py
@@ -4,17 +4,14 @@ from typing import List
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         # Time O(n log n), Memory O(n)
-        sorted_intervals = sorted(intervals, key=lambda x: x[0])
-        curr = sorted_intervals[0]
+        intervals.sort(key=lambda x: x[0])
+        curr = intervals[0]
         res = []
         for i in range(1, len(intervals)):
-            if curr[1] < sorted_intervals[i][0]:
+            if curr[1] < intervals[i][0]:
                 res.append(curr)
-                curr = sorted_intervals[i]
+                curr = intervals[i]
             else:
-                curr = [
-                    min(curr[0], sorted_intervals[i][0]),
-                    max(curr[1], sorted_intervals[i][1]),
-                ]
+                curr = [min(curr[0], intervals[i][0]), max(curr[1], intervals[i][1])]
         res.append(curr)
         return res
