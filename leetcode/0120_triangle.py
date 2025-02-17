2450d5cead064ab815c4ad418f1b2ec700903de3
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-02-17 18:22:54 +0000

    dp solution to save on repeated work

--- leetcode/0120_triangle.py
+++ leetcode/0120_triangle.py
@@ -2,17 +2,29 @@ from typing import List
 
 
 class Solution:
-    # Too slow
+    # dynamic programming
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        # Time O(2^n), Memory O(n) where n = #rows
-        def dfs(row_index, level):  # post order traversal but repeated work
-            if level == (len(triangle) - 1):
-                return triangle[level][row_index]
-            l = dfs(row_index, level + 1)
-            r = dfs(row_index + 1, level + 1)
-            return triangle[level][row_index] + min(l, r)
+        # Time O(n^2), Memory O(n), where n=#rows
+        for level in range(len(triangle) - 2, -1, -1):
+            for row_index in range(level + 1):
+                triangle[level][row_index] += min(
+                    triangle[level + 1][row_index], triangle[level + 1][row_index + 1]
+                )
+        return triangle[0][0]
 
-        return dfs(0, 0)
+
+# class Solution:
+#     # Too slow
+#     def minimumTotal(self, triangle: List[List[int]]) -> int:
+#         # Time O(2^n), Memory O(n) where n = #rows
+#         def dfs(row_index, level):  # post order traversal but repeated work
+#             if level == (len(triangle) - 1):
+#                 return triangle[level][row_index]
+#             l = dfs(row_index, level + 1)
+#             r = dfs(row_index + 1, level + 1)
+#             return triangle[level][row_index] + min(l, r)
+
+#         return dfs(0, 0)
 
 
 # class Solution:
