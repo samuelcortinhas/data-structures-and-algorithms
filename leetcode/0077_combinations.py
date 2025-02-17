c31381e2f80582a3e39e24eee9c39fe365fcfe99
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-02-17 23:01:51 +0000

    optimise by passing left boundary for next value in combination

--- leetcode/0077_combinations.py
+++ leetcode/0077_combinations.py
@@ -3,18 +3,37 @@ from typing import List
 
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        # Time O(n^k), Memory O(k * nCk)
+        # Time O(k * nCk), Memory O(k * nCk)
         res = []
 
-        def backtrack(i, stack):
-            if i == k:
+        def backtrack(prev, stack):
+            if len(stack) == k:
                 res.append(list(stack))
+                return
 
-            for j in range(1, n + 1):
-                if not stack or j > stack[-1]:
-                    stack.append(j)
-                    backtrack(i + 1, stack)
-                    stack.pop()
+            for j in range(prev + 1, n + 1):
+                stack.append(j)
+                backtrack(j, stack)
+                stack.pop()
 
         backtrack(0, [])
         return res
+
+
+# class Solution:
+#     def combine(self, n: int, k: int) -> List[List[int]]:
+#         # Time O(n^k), Memory O(k * nCk)
+#         res = []
+
+#         def backtrack(i, stack):
+#             if i == k:
+#                 res.append(list(stack))
+
+#             for j in range(1, n + 1):
+#                 if not stack or j > stack[-1]:
+#                     stack.append(j)
+#                     backtrack(i + 1, stack)
+#                     stack.pop()
+
+#         backtrack(0, [])
+#         return res
