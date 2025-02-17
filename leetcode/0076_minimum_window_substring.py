148cf4a9d5c8bd8b2b275f038524d2e8ac627459
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-02-17 11:19:25 +0000

    unused method

--- leetcode/0076_minimum_window_substring.py
+++ leetcode/0076_minimum_window_substring.py
@@ -2,13 +2,6 @@ from collections import Counter, defaultdict
 
 
 class Solution:
-    @staticmethod
-    def is_valid(counts1, counts2):
-        for k, v in counts2.items():
-            if counts1[k] < v:
-                return False
-        return True
-
     def minWindowV2(self, s: str, t: str) -> str:
         # Time O(s+t), Memory O(s+t)
         s_counts = defaultdict(int)
@@ -31,6 +24,13 @@ class Solution:
             if have == need and right - left < len(res):
                 res = s[left:right]
         return res if len(res) <= len(s) else ""
+
+    # @staticmethod
+    # def is_valid(counts1, counts2):
+    #     for k, v in counts2.items():
+    #         if counts1[k] < v:
+    #             return False
+    #     return True
 
     # def minWindowV1(self, s: str, t: str) -> str:
     #     # Time O(s*t), Memory O(s+t)
