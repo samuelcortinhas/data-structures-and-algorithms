c86da953e2d3435f9adf69f21f71ab642525b313
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-02-17 22:24:43 +0000

    revist and improve

--- leetcode/0090_subsets_ii.py
+++ leetcode/0090_subsets_ii.py
@@ -7,21 +7,46 @@ class Solution:
         res = []
         nums.sort()
 
-        def dfs(i, stack, stack_sum):
+        def dfs(i, stack):
             if i == len(nums):
                 res.append(list(stack))
                 return
 
             # decision to include nums[i]
             stack.append(nums[i])
-            stack_sum += nums[i]
-            dfs(i + 1, stack, stack_sum)
+            dfs(i + 1, stack)
+            stack.pop()
 
-            # decision to skip nums[i]
+            # decision to skip nums[i] (and it's duplicates)
             while i + 1 < len(nums) and nums[i + 1] == nums[i]:
                 i += 1
-            stack_sum -= stack.pop()
-            dfs(i + 1, stack, stack_sum)
+            dfs(i + 1, stack)
 
-        dfs(0, [], 0)
+        dfs(0, [])
         return res
+
+
+# class Solution:
+#     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+#         # Time O(n 2^n), Memory O(n 2^n)
+#         res = []
+#         nums.sort()
+
+#         def dfs(i, stack, stack_sum):
+#             if i == len(nums):
+#                 res.append(list(stack))
+#                 return
+
+#             # decision to include nums[i]
+#             stack.append(nums[i])
+#             stack_sum += nums[i]
+#             dfs(i + 1, stack, stack_sum)
+
+#             # decision to skip nums[i]
+#             while i + 1 < len(nums) and nums[i + 1] == nums[i]:
+#                 i += 1
+#             stack_sum -= stack.pop()
+#             dfs(i + 1, stack, stack_sum)
+
+#         dfs(0, [], 0)
+#         return res
