b759d130142e1b9c911ef81060e3c818268fe856
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-02-17 11:17:07 +0000

    optimise by tracking how many characters have been seen enough in window

--- leetcode/0076_minimum_window_substring.py
+++ leetcode/0076_minimum_window_substring.py
@@ -9,20 +9,43 @@ class Solution:
                 return False
         return True
 
-    def minWindow(self, s: str, t: str) -> str:
-        # Time O(s*t), Memory O(s+t)
+    def minWindowV2(self, s: str, t: str) -> str:
+        # Time O(s+t), Memory O(s+t)
         s_counts = defaultdict(int)
         t_counts = Counter(t)
         left, right = 0, 0
         res = s + "a"
+        have, need = 0, len(t_counts)
         while right < len(s) + 1:
-            if left < right and s_counts[s[left]] > t_counts[s[left]]:
+            if left < right and have == need:
                 s_counts[s[left]] -= 1
+                if s_counts[s[left]] == t_counts[s[left]] - 1:
+                    have -= 1
                 left += 1
             else:
                 if right < len(s):
                     s_counts[s[right]] += 1
+                    if s_counts[s[right]] == t_counts[s[right]]:
+                        have += 1
                 right += 1
-            if self.is_valid(s_counts, t_counts) and right - left < len(res):
+            if have == need and right - left < len(res):
                 res = s[left:right]
         return res if len(res) <= len(s) else ""
+
+    # def minWindowV1(self, s: str, t: str) -> str:
+    #     # Time O(s*t), Memory O(s+t)
+    #     s_counts = defaultdict(int)
+    #     t_counts = Counter(t)
+    #     left, right = 0, 0
+    #     res = s + "a"
+    #     while right < len(s) + 1:
+    #         if left < right and s_counts[s[left]] > t_counts[s[left]]:
+    #             s_counts[s[left]] -= 1
+    #             left += 1
+    #         else:
+    #             if right < len(s):
+    #                 s_counts[s[right]] += 1
+    #             right += 1
+    #         if self.is_valid(s_counts, t_counts) and right - left < len(res):
+    #             res = s[left:right]
+    #     return res if len(res) <= len(s) else ""
