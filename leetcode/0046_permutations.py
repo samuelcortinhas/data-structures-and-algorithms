a3140171c2fd95de362729a86ca67999e6c8eb2c
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-02-18 11:32:46 +0000

    revisit - use less global vars this time

--- leetcode/0046_permutations.py
+++ leetcode/0046_permutations.py
@@ -6,16 +6,35 @@ class Solution:
         # Time O(n^n), Memory O(n!)
         res = []
 
-        def dfs(stack):
+        def backtrack(i, stack):
             if len(stack) == len(nums):
                 res.append(list(stack))
-                return
 
-            for j in range(len(nums)):
-                if nums[j] not in stack:
-                    stack.append(nums[j])
-                    dfs(stack)
+            for n in nums:
+                if n not in stack:
+                    stack.append(n)
+                    backtrack(i + 1, stack)
                     stack.pop()
 
-        dfs([])
+        backtrack(0, [])
         return res
+
+
+# class Solution:
+#     def permute(self, nums: List[int]) -> List[List[int]]:
+#         # Time O(n^n), Memory O(n!)
+#         res = []
+
+#         def dfs(stack):
+#             if len(stack) == len(nums):
+#                 res.append(list(stack))
+#                 return
+
+#             for j in range(len(nums)):
+#                 if nums[j] not in stack:
+#                     stack.append(nums[j])
+#                     dfs(stack)
+#                     stack.pop()
+
+#         dfs([])
+#         return res
