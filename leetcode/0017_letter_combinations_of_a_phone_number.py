34da1aadd487d24591752ae273d94ac3a2def817
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-02-18 11:24:14 +0000

    revisit and slightly improve solution

--- leetcode/0017_letter_combinations_of_a_phone_number.py
+++ leetcode/0017_letter_combinations_of_a_phone_number.py
@@ -14,18 +14,47 @@ class Solution:
             "8": "tuv",
             "9": "wxyz",
         }
-        nums = list(digits)
         res = []
-        stack = []
 
-        def dfs(i):
-            if i >= len(digits):
-                res.append(list(stack))
+        def backtrack(i, stack):
+            if len(stack) == len(digits):
+                res.append("".join(stack))
                 return
-            for char in phone_map[nums[i]]:
-                stack.append(char)
-                dfs(i + 1)
+
+            for c in phone_map[digits[i]]:
+                stack.append(c)
+                backtrack(i + 1, stack)
                 stack.pop()
 
-        dfs(0)
-        return ["".join(r) for r in res if r]
+        backtrack(0, [])
+        return res if digits else []
+
+
+# class Solution:
+#     def letterCombinations(self, digits: str) -> List[str]:
+#         # Time O(4^n), Memory O(4^n)
+#         phone_map = {
+#             "2": "abc",
+#             "3": "def",
+#             "4": "ghi",
+#             "5": "jkl",
+#             "6": "mno",
+#             "7": "pqrs",
+#             "8": "tuv",
+#             "9": "wxyz",
+#         }
+#         nums = list(digits)
+#         res = []
+#         stack = []
+
+#         def dfs(i):
+#             if i >= len(digits):
+#                 res.append(list(stack))
+#                 return
+#             for char in phone_map[nums[i]]:
+#                 stack.append(char)
+#                 dfs(i + 1)
+#                 stack.pop()
+
+#         dfs(0)
+#         return ["".join(r) for r in res if r]
