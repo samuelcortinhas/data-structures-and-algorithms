f30d52da84d0851b64b2023b8f4194731122a14b
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-02-18 19:35:12 +0000

    optimise building of adj_list by using pattern matching

--- leetcode/0127_word_ladder.py
+++ leetcode/0127_word_ladder.py
@@ -4,25 +4,20 @@ from typing import List
 
 class Solution:
     @staticmethod
-    def connected(word1, word2):
-        count = 0
-        for c1, c2 in zip(word1, word2):
-            if c1 != c2:
-                count += 1
-        return count == 1
+    def get_patterns(word):
+        res = []
+        for j in range(len(word)):
+            res.append(word[:j] + "*" + word[j + 1 :])
+        return res
 
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        # Time O(n^2 m), Memory O(n^2 * m) where n = len(wordList), m = len(endWord)
-
         # build graph where words are connected if they differ by 1 letter
-        adj_list = defaultdict(list)
+        adj_list = defaultdict(list)  # Time O(n m^2)
         wordList += [beginWord]
-        for i1 in range(len(wordList)):
-            for i2 in range(i1 + 1, len(wordList)):
-                word1, word2 = wordList[i1], wordList[i2]
-                if self.connected(word1, word2):
-                    adj_list[word1].append(word2)
-                    adj_list[word2].append(word1)
+        for word in wordList:
+            patterns = self.get_patterns(word)
+            for p in patterns:
+                adj_list[p].append(word)
 
         # do bfs on this graph
         queue = deque()
@@ -37,7 +32,50 @@ class Solution:
                     return res
                 visit.add(curr_word)
 
-                for neigh in adj_list[curr_word]:
-                    if neigh not in visit:
-                        queue.append(neigh)
+                patterns = self.get_patterns(curr_word)
+                for p in patterns:
+                    for neigh in adj_list[p]:
+                        if neigh not in visit:
+                            queue.append(neigh)
         return 0
+
+
+# class Solution:
+#     @staticmethod
+#     def connected(word1, word2):
+#         count = 0
+#         for c1, c2 in zip(word1, word2):
+#             if c1 != c2:
+#                 count += 1
+#         return count == 1
+
+#     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+#         # Time O(n^2 m), Memory O(n^2 * m) where n = len(wordList), m = len(endWord)
+
+#         # build graph where words are connected if they differ by 1 letter
+#         adj_list = defaultdict(list)
+#         wordList += [beginWord]
+#         for i1 in range(len(wordList)):
+#             for i2 in range(i1 + 1, len(wordList)):
+#                 word1, word2 = wordList[i1], wordList[i2]
+#                 if self.connected(word1, word2):
+#                     adj_list[word1].append(word2)
+#                     adj_list[word2].append(word1)
+
+#         # do bfs on this graph
+#         queue = deque()
+#         queue.append(beginWord)
+#         visit = set()
+#         res = 0
+#         while queue:
+#             res += 1
+#             for _ in range(len(queue)):
+#                 curr_word = queue.popleft()
+#                 if curr_word == endWord:
+#                     return res
+#                 visit.add(curr_word)
+
+#                 for neigh in adj_list[curr_word]:
+#                     if neigh not in visit:
+#                         queue.append(neigh)
+#         return 0
