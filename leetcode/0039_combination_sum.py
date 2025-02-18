f92cfeed503493b7c2123b11320691bcda8ff6c8
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-02-18 11:12:48 +0000

    slightly different backtracking approach

--- leetcode/0039_combination_sum.py
+++ leetcode/0039_combination_sum.py
@@ -2,52 +2,76 @@ from typing import List
 
 
 class Solution:
-    # Time O(t 2^t), Memory O(t 2^t) where t is the target
+    # Time O(t n^t), Memory O(t n^t) where t is the target
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        # backtracking
         res = []
 
-        def dfs(i, stack, stack_sum):
-            # i = index of number being considered
-            if stack_sum == target:
+        def backtrack(prev_index, stack, curr_sum):
+            if curr_sum == target:
                 res.append(list(stack))
                 return
-            elif stack_sum > target or i >= len(candidates):
+            if curr_sum > target:
                 return
 
-            # decision to include candidates[i] in stack
-            stack.append(candidates[i])
-            stack_sum += candidates[i]
-            dfs(i, stack, stack_sum)
+            for i in range(prev_index, len(candidates)):
+                c = candidates[i]
+                stack.append(c)
+                curr_sum += c
+                backtrack(i, stack, curr_sum)
+                curr_sum -= c
+                stack.pop()
 
-            # decision not to include candidates[i] in stack
-            stack_sum -= stack.pop()
-            dfs(i + 1, stack, stack_sum)
-
-        dfs(0, [], 0)
+        backtrack(0, [], 0)
         return res
 
-    # def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-    #     # backtracking
-    #     res = []
-    #     stack = []
 
-    #     def dfs(i):
-    #         # i = index of number being considered
-    #         s = sum(stack)
-    #         if s == target:
-    #             res.append(list(stack))
-    #             return
-    #         elif s > target or i >= len(candidates):
-    #             return
+# class Solution:
+#     # Time O(t 2^t), Memory O(t 2^t) where t is the target
+#     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+#         # backtracking
+#         res = []
 
-    #         # decision to include candidates[i] in stack
-    #         stack.append(candidates[i])
-    #         dfs(i)
+#         def dfs(i, stack, stack_sum):
+#             # i = index of number being considered
+#             if stack_sum == target:
+#                 res.append(list(stack))
+#                 return
+#             elif stack_sum > target or i >= len(candidates):
+#                 return
 
-    #         # decision not to include candidates[i] in stack
-    #         stack.pop()
-    #         dfs(i + 1)
+#             # decision to include candidates[i] in stack
+#             stack.append(candidates[i])
+#             stack_sum += candidates[i]
+#             dfs(i, stack, stack_sum)
 
-    #     dfs(0)
-    #     return res
+#             # decision not to include candidates[i] in stack
+#             stack_sum -= stack.pop()
+#             dfs(i + 1, stack, stack_sum)
+
+#         dfs(0, [], 0)
+#         return res
+
+# def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+#     # backtracking
+#     res = []
+#     stack = []
+
+#     def dfs(i):
+#         # i = index of number being considered
+#         s = sum(stack)
+#         if s == target:
+#             res.append(list(stack))
+#             return
+#         elif s > target or i >= len(candidates):
+#             return
+
+#         # decision to include candidates[i] in stack
+#         stack.append(candidates[i])
+#         dfs(i)
+
+#         # decision not to include candidates[i] in stack
+#         stack.pop()
+#         dfs(i + 1)
+
+#     dfs(0)
+#     return res
