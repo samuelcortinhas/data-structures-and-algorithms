09bacc27e411ad6ed145d87357909432c9ab5b72
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-02-19 22:38:39 +0000

    compare consecutive words, return false if order not preserved else return true

---
+++ leetcode/0953_verify_alien_dictionary.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    # Time O(m*n), Memory O(26), where n=len(words), m=max(len(word))
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        # compare consecutive words
+        char_index = {}  # character: index
+        for i, c in enumerate(order):
+            char_index[c] = i
+
+        for j in range(1, len(words)):
+            skip = False
+            for a, b in zip(words[j - 1], words[j]):
+                if char_index[a] < char_index[b]:
+                    skip = True
+                    break
+                elif char_index[a] > char_index[b]:
+                    return False
+            if not skip and len(words[j - 1]) > len(words[j]):
+                return False
+        return True
