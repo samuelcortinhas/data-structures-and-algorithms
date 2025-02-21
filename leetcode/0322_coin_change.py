cee7051b59875523e0fbec59adb624caa45d43a5
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-02-21 17:33:08 +0000

    top down dp with greedy and backtracking with cache

--- leetcode/0322_coin_change.py
+++ leetcode/0322_coin_change.py
@@ -2,14 +2,33 @@ from typing import List
 
 
 class Solution:
-    # Greedy solution - does not work in all cases
+    # top down dp: greedy + backtracking via dfs + cache
     def coinChange(self, coins: List[int], amount: int) -> int:
-        # Time O(amount/k), Memory O(1) where k = min(coins)
+        # Time O(m*n), Memory O(m), where n=len(coins), m=amount
+        dp = {0: 0}
         coins.sort(reverse=True)
-        total = 0
-        count = 0
-        for i in range(len(coins)):
-            while total + coins[i] <= amount:
-                count += 1
-                total += coins[i]
-        return count if total == amount else -1
+
+        def dfs(remaining):
+            if remaining in dp:
+                return dp[remaining]
+            for c in coins:
+                if remaining - c >= 0:
+                    dp[remaining] = min(
+                        dp.get(remaining, float("inf")), 1 + dfs(remaining - c)
+                    )
+            return dp.get(remaining, float("inf"))
+
+        dfs(amount)
+        return dp.get(amount, -1) if dp.get(amount, -1) != float("inf") else -1
+
+    # # Greedy solution - does not work in all cases
+    # def coinChange(self, coins: List[int], amount: int) -> int:
+    #     # Time O(amount/k), Memory O(1) where k = min(coins)
+    #     coins.sort(reverse=True)
+    #     total = 0
+    #     count = 0
+    #     for i in range(len(coins)):
+    #         while total + coins[i] <= amount:
+    #             count += 1
+    #             total += coins[i]
+    #     return count if total == amount else -1
