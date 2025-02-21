e4a2ace4eb5967691bdc21429a162372f1f2ba60
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-02-21 12:00:00 +0000

    better variable names

--- leetcode/0213_house_robber_ii.py
+++ leetcode/0213_house_robber_ii.py
@@ -4,12 +4,24 @@ from typing import List
 class Solution:
     def rob(self, nums: List[int]) -> int:
         # Time O(n), Memory O(1)
-        def houserobber(nums):
-            prev_one, prev_two = 0, 0
-            for i in range(len(nums)):
-                bank = max(nums[i] + prev_two, prev_one)
-                prev_two = prev_one
-                prev_one = bank
-            return prev_one
+        def house_robber(nums):
+            prev, curr = 0, 0
+            for n in nums:
+                tmp = curr
+                curr = max(prev + n, curr)
+                prev = tmp
+            return curr
 
-        return max(houserobber(nums[1:]), houserobber(nums[:-1]), nums[0])
+        return max(house_robber(nums[1:]), house_robber(nums[:-1]), nums[0])
+
+    # def rob(self, nums: List[int]) -> int:
+    #     # Time O(n), Memory O(1)
+    #     def houserobber(nums):
+    #         prev_one, prev_two = 0, 0
+    #         for i in range(len(nums)):
+    #             bank = max(nums[i] + prev_two, prev_one)
+    #             prev_two = prev_one
+    #             prev_one = bank
+    #         return prev_one
+
+    #     return max(houserobber(nums[1:]), houserobber(nums[:-1]), nums[0])
