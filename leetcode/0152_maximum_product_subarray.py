a36e6ede83393aa38080ae4d5ca0680b1a468f41
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-02-21 18:20:10 +0000

    quadratic dp solution - still too slow

--- leetcode/0152_maximum_product_subarray.py
+++ leetcode/0152_maximum_product_subarray.py
@@ -2,15 +2,27 @@ from typing import List
 
 
 class Solution:
-    # Brute force
+    # dp solution - too slow
     def maxProduct(self, nums: List[int]) -> int:
-        # Time O(n^3), Memory O(1)
-        res = float("-inf")
+        # Time O(n^2), Memory O(1)
+        res = max(nums)
         for i in range(len(nums)):
-            for j in range(i, len(nums)):
-                prod = 1
-                for p in nums[i : j + 1]:
-                    prod *= p
+            prod = nums[i]
+            for j in range(i + 1, len(nums)):
+                prod *= nums[j]
                 if prod > res:
                     res = prod
         return res
+
+    # Brute force - too slow
+    # def maxProduct(self, nums: List[int]) -> int:
+    #     # Time O(n^3), Memory O(1)
+    #     res = float("-inf")
+    #     for i in range(len(nums)):
+    #         for j in range(i, len(nums)):
+    #             prod = 1
+    #             for p in nums[i : j + 1]:
+    #                 prod *= p
+    #             if prod > res:
+    #                 res = prod
+    #     return res
