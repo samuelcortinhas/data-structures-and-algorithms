7aaa300a3b557b219cdd93e74d2be019b37d0423
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-02-22 14:03:09 +0000

    top down dp solution

--- leetcode/0416_partition_equal_subset_sum.py
+++ leetcode/0416_partition_equal_subset_sum.py
@@ -2,6 +2,34 @@ from typing import List
 
 
 class Solution:
+    # top down dp - too slow
+    def canPartition(self, nums: List[int]) -> bool:
+        # Time O(2^n), Memory O(n)
+        target = sum(nums) / 2
+        if sum(nums) % 2:
+            return False
+        dp = [False] * (len(nums) + 1)
+
+        def dfs(i, curr_sum):
+            if dp[i]:
+                return True
+            if curr_sum == target:
+                dp[i] = True
+                return True
+            if i >= len(nums):
+                return False
+
+            curr_sum += nums[i]
+            if dfs(i + 1, curr_sum):
+                return True
+            curr_sum -= nums[i]
+
+            if dfs(i + 1, curr_sum):
+                return True
+            return False
+
+        return dfs(0, 0)
+
     # dfs with backtracking - too slow
     def canPartition(self, nums: List[int]) -> bool:
         # Time O(2^n), Memory O(n)
