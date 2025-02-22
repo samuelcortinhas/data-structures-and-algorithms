43d5c448c762a02383061111b49eb0e4eba3c056
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-02-22 14:32:11 +0000

    bottom up dp solution

--- leetcode/0416_partition_equal_subset_sum.py
+++ leetcode/0416_partition_equal_subset_sum.py
@@ -2,54 +2,72 @@ from typing import List
 
 
 class Solution:
-    # top down dp - too slow
+    # bottom up dp
     def canPartition(self, nums: List[int]) -> bool:
-        # Time O(2^n), Memory O(n)
-        target = sum(nums) / 2
-        if sum(nums) % 2:
-            return False
-        dp = [False] * (len(nums) + 1)
-
-        def dfs(i, curr_sum):
-            if dp[i]:
-                return True
-            if curr_sum == target:
-                dp[i] = True
-                return True
-            if i >= len(nums):
-                return False
-
-            curr_sum += nums[i]
-            if dfs(i + 1, curr_sum):
-                return True
-            curr_sum -= nums[i]
-
-            if dfs(i + 1, curr_sum):
-                return True
-            return False
-
-        return dfs(0, 0)
-
-    # dfs with backtracking - too slow
-    def canPartition(self, nums: List[int]) -> bool:
-        # Time O(2^n), Memory O(n)
-        target = sum(nums) / 2
+        # Time O(n * target), Memory O(target)
         if sum(nums) % 2:
             return False
 
-        def dfs(i, curr_sum):
-            if curr_sum == target:
+        target = sum(nums) / 2
+        dp = set([0])
+        for i in range(len(nums) - 1, -1, -1):
+            new = set()
+            for s in dp:
+                if s + nums[i] <= target:
+                    new.add(s + nums[i])
+            dp = dp.union(new)
+            if target in dp:
                 return True
-            if i >= len(nums):
-                return False
+        return False
 
-            curr_sum += nums[i]
-            if dfs(i + 1, curr_sum):
-                return True
-            curr_sum -= nums[i]
+    # # top down dp - too slow
+    # def canPartition(self, nums: List[int]) -> bool:
+    #     # Time O(2^n), Memory O(n)
+    #     target = sum(nums) / 2
+    #     if sum(nums) % 2:
+    #         return False
+    #     dp = [False] * (len(nums) + 1)
 
-            if dfs(i + 1, curr_sum):
-                return True
-            return False
+    #     def dfs(i, curr_sum):
+    #         if dp[i]:
+    #             return True
+    #         if curr_sum == target:
+    #             dp[i] = True
+    #             return True
+    #         if i >= len(nums):
+    #             return False
 
-        return dfs(0, 0)
+    #         curr_sum += nums[i]
+    #         if dfs(i + 1, curr_sum):
+    #             return True
+    #         curr_sum -= nums[i]
+
+    #         if dfs(i + 1, curr_sum):
+    #             return True
+    #         return False
+
+    #     return dfs(0, 0)
+
+    # # dfs with backtracking - too slow
+    # def canPartition(self, nums: List[int]) -> bool:
+    #     # Time O(2^n), Memory O(n)
+    #     target = sum(nums) / 2
+    #     if sum(nums) % 2:
+    #         return False
+
+    #     def dfs(i, curr_sum):
+    #         if curr_sum == target:
+    #             return True
+    #         if i >= len(nums):
+    #             return False
+
+    #         curr_sum += nums[i]
+    #         if dfs(i + 1, curr_sum):
+    #             return True
+    #         curr_sum -= nums[i]
+
+    #         if dfs(i + 1, curr_sum):
+    #             return True
+    #         return False
+
+    #     return dfs(0, 0)
