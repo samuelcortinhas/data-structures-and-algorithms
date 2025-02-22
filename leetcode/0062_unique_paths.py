0311ee7a84bdda13cbf0a92adf3d9c7eb969153d
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-02-22 16:42:06 +0000

    memory efficient bottom up dp solution

--- leetcode/0062_unique_paths.py
+++ leetcode/0062_unique_paths.py
@@ -1,18 +1,29 @@
 class Solution:
-    # bottom up dp - iterate in reverse
+    # bottom up dp - memory efficient
     def uniquePaths(self, m: int, n: int) -> int:
-        # Time O(m*n), Memory O(m*n)
-        dp = [[0] * n for _ in range(m)]
-        dp[m - 1][n - 1] = 1
+        prev_row = [0] * n
         for i in range(m - 1, -1, -1):
-            for j in range(n - 1, -1, -1):
-                if i < m - 1 and j < n - 1:
-                    dp[i][j] = dp[i + 1][j] + dp[i][j + 1]
-                elif i < m - 1:
-                    dp[i][j] = dp[i + 1][j]
-                elif j < n - 1:
-                    dp[i][j] = dp[i][j + 1]
-        return dp[0][0]
+            curr_row = [0] * n
+            curr_row[-1] = 1
+            for j in range(n - 2, -1, -1):
+                curr_row[j] = curr_row[j + 1] + prev_row[j]
+            prev_row = curr_row
+        return curr_row[0]
+
+    # # bottom up dp - iterate in reverse
+    # def uniquePaths(self, m: int, n: int) -> int:
+    #     # Time O(m*n), Memory O(m*n)
+    #     dp = [[0] * n for _ in range(m)]
+    #     dp[m - 1][n - 1] = 1
+    #     for i in range(m - 1, -1, -1):
+    #         for j in range(n - 1, -1, -1):
+    #             if i < m - 1 and j < n - 1:
+    #                 dp[i][j] = dp[i + 1][j] + dp[i][j + 1]
+    #             elif i < m - 1:
+    #                 dp[i][j] = dp[i + 1][j]
+    #             elif j < n - 1:
+    #                 dp[i][j] = dp[i][j + 1]
+    #     return dp[0][0]
 
     # # top down dp - dfs with cache
     # def uniquePaths(self, m: int, n: int) -> int:
