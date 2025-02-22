1d832c9139df84707472a114ac0cd1a3ab96f75f
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-02-22 12:35:40 +0000

    bottum up dp solution

---
+++ leetcode/0300_longest_increasing_subsequence.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    # bottom up dp solution
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        # Time O(n^2), Memory O(n)
+        dp = [1] * len(nums)
+        for i in range(len(nums) - 2, -1, -1):
+            for j in range(i, len(nums)):
+                if nums[i] < nums[j]:
+                    dp[i] = max(dp[i], 1 + dp[j])
+        return max(dp)
