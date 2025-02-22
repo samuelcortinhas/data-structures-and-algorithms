9599546e2dbf81fafdbf5cda6396371522fa2a6f
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-02-22 16:35:09 +0000

    bottom up dp - iterate in reverse

--- leetcode/0062_unique_paths.py
+++ leetcode/0062_unique_paths.py
@@ -1,21 +1,36 @@
 class Solution:
-    # top down dp - dfs with cache
+    # bottom up dp - iterate in reverse
     def uniquePaths(self, m: int, n: int) -> int:
         # Time O(m*n), Memory O(m*n)
         dp = [[0] * n for _ in range(m)]
+        dp[m - 1][n - 1] = 1
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+                if i < m - 1 and j < n - 1:
+                    dp[i][j] = dp[i + 1][j] + dp[i][j + 1]
+                elif i < m - 1:
+                    dp[i][j] = dp[i + 1][j]
+                elif j < n - 1:
+                    dp[i][j] = dp[i][j + 1]
+        return dp[0][0]
 
-        def dfs(i, j):
-            if dp[i][j]:
-                return dp[i][j]
-            if i == m or j == n:
-                return 0
-            if i == m - 1 or j == n - 1:
-                return 1
-            res = dfs(i + 1, j) + dfs(i, j + 1)
-            dp[i][j] = res
-            return res
+    # # top down dp - dfs with cache
+    # def uniquePaths(self, m: int, n: int) -> int:
+    #     # Time O(m*n), Memory O(m*n)
+    #     dp = [[0] * n for _ in range(m)]
 
-        return dfs(0, 0)
+    #     def dfs(i, j):
+    #         if dp[i][j]:
+    #             return dp[i][j]
+    #         if i == m or j == n:
+    #             return 0
+    #         if i == m - 1 or j == n - 1:
+    #             return 1
+    #         res = dfs(i + 1, j) + dfs(i, j + 1)
+    #         dp[i][j] = res
+    #         return res
+
+    #     return dfs(0, 0)
 
     # # dfs - too slow
     # def uniquePaths(self, m: int, n: int) -> int:
