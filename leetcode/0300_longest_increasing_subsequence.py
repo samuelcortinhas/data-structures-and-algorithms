0d30dbb38e6474a5c0a001520abf1dd2d9d5afe4
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-02-22 12:57:55 +0000

    small optimisation

--- leetcode/0300_longest_increasing_subsequence.py
+++ leetcode/0300_longest_increasing_subsequence.py
@@ -7,7 +7,7 @@ class Solution:
         # Time O(n^2), Memory O(n)
         dp = [1] * len(nums)
         for i in range(len(nums) - 2, -1, -1):
-            for j in range(i, len(nums)):
+            for j in range(i + 1, len(nums)):
                 if nums[i] < nums[j]:
                     dp[i] = max(dp[i], 1 + dp[j])
         return max(dp)
