c680d72d6a377f6bae8ebb7717ef8446b1fad498
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-02-23 13:52:27 +0000

    cache max profit for can_buy and not can_buy state

--- leetcode/0309_best_time_to_buy_and_sell_stock_with_cooldown.py
+++ leetcode/0309_best_time_to_buy_and_sell_stock_with_cooldown.py
@@ -3,32 +3,54 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # actions: buy, sell, cooldown or hold
-        # dfs + backtracking on these actions
-        # Time O(4^n), Memory O(n)
-        res = 0
+        # Time O(n), Memory O(n)
+        dp = {}  # (index, can_buy): max_profit
 
-        def dfs(i, action, profit):
-            nonlocal res
+        def dfs(i, can_buy):
             if i >= len(prices):
-                res = max(res, profit)
-                return
+                return 0
+            if (i, can_buy) in dp:
+                return dp[(i, can_buy)]
 
-            if action == "buy":
-                dfs(i + 1, "sell", profit - prices[i])
-                dfs(i + 1, "hold", profit)
+            if can_buy:
+                buy = dfs(i + 1, not can_buy) - prices[i]
+                hold = dfs(i + 1, can_buy)
+                dp[(i, can_buy)] = max(buy, hold)
+            else:
+                sell = dfs(i + 2, not can_buy) + prices[i]
+                hold = dfs(i + 1, can_buy)
+                dp[(i, can_buy)] = max(sell, hold)
+            return dp[(i, can_buy)]
 
-            elif action == "sell":
-                dfs(i + 1, "cooldown", profit + prices[i])
+        return dfs(0, True)
 
-            elif action == "hold":
-                dfs(i + 1, "sell", profit)
-                dfs(i + 1, "hold", profit)
+    # def maxProfit(self, prices: List[int]) -> int:
+    #     # actions: buy, sell, cooldown or hold
+    #     # dfs + backtracking on these actions
+    #     # Time O(4^n), Memory O(n)
+    #     res = 0
 
-            elif action == "cooldown":
-                dfs(i + 1, "buy", profit)
-                dfs(i + 1, "cooldown", profit)
+    #     def dfs(i, action, profit):
+    #         nonlocal res
+    #         if i >= len(prices):
+    #             res = max(res, profit)
+    #             return
 
-        dfs(0, "buy", 0)
-        dfs(0, "cooldown", 0)
-        return res
+    #         if action == "buy":
+    #             dfs(i + 1, "sell", profit - prices[i])
+    #             dfs(i + 1, "hold", profit)
+
+    #         elif action == "sell":
+    #             dfs(i + 1, "cooldown", profit + prices[i])
+
+    #         elif action == "hold":
+    #             dfs(i + 1, "sell", profit)
+    #             dfs(i + 1, "hold", profit)
+
+    #         elif action == "cooldown":
+    #             dfs(i + 1, "buy", profit)
+    #             dfs(i + 1, "cooldown", profit)
+
+    #     dfs(0, "buy", 0)
+    #     dfs(0, "cooldown", 0)
+    #     return res
