61f407ccd92362fa1fea511a3edcc55710d2f618
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-02-24 18:49:37 +0000

    top down dp solution using dict cache

--- leetcode/0494_target_sum.py
+++ leetcode/0494_target_sum.py
@@ -2,6 +2,25 @@ from typing import List
 
 
 class Solution:
+    # top down dp solution - cache curr_target at each index
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        # Time O(2^n), Memory O(n*target)
+        dp = {}
+
+        def dfs(i, curr_target):
+            if (i, curr_target) in dp:
+                return dp[(i, curr_target)]
+            if i == len(nums) and curr_target == 0:
+                return 1
+            elif i == len(nums):
+                return 0
+            dp[(i, curr_target)] = dfs(i + 1, curr_target - nums[i]) + dfs(
+                i + 1, curr_target + nums[i]
+            )
+            return dp[(i, curr_target)]
+
+        return dfs(0, target)
+
     # brute force - too slow
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         # Time O(2^n), Memory O(2^n) where n=len(nums)
