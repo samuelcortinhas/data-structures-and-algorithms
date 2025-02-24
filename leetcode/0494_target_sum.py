f0708da635097228130332bf6c1c461074f71d64
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-02-24 20:51:35 +0000

    true dp solution directly iterating over nums and computing counts for each curr_sum

--- leetcode/0494_target_sum.py
+++ leetcode/0494_target_sum.py
@@ -1,25 +1,39 @@
+from collections import defaultdict
 from typing import List
 
 
 class Solution:
-    # top down dp solution - cache curr_target at each index
+    # true dp solution
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        # Time O(n*m), Memory O(n*m) where n=len(nums), m=sum(abs(nums))
-        dp = {}
+        # Time O(n^2), Memory O(n)
+        dp = defaultdict(int)
+        dp[0] = 1  # curr_sum : count of ways to get curr_sum
+        for i in range(len(nums)):
+            next_dp = defaultdict(int)
+            for curr_sum, count in dp.items():
+                next_dp[curr_sum + nums[i]] += count
+                next_dp[curr_sum - nums[i]] += count
+            dp = next_dp
+        return dp[target]
 
-        def dfs(i, curr_target):
-            if (i, curr_target) in dp:
-                return dp[(i, curr_target)]
-            if i == len(nums) and curr_target == 0:
-                return 1
-            elif i == len(nums):
-                return 0
-            dp[(i, curr_target)] = dfs(i + 1, curr_target - nums[i]) + dfs(
-                i + 1, curr_target + nums[i]
-            )
-            return dp[(i, curr_target)]
+    # # top down dp solution - cache curr_target at each index
+    # def findTargetSumWays(self, nums: List[int], target: int) -> int:
+    #     # Time O(n*m), Memory O(n*m) where n=len(nums), m=sum(abs(nums))
+    #     dp = {}
 
-        return dfs(0, target)
+    #     def dfs(i, curr_target):
+    #         if (i, curr_target) in dp:
+    #             return dp[(i, curr_target)]
+    #         if i == len(nums) and curr_target == 0:
+    #             return 1
+    #         elif i == len(nums):
+    #             return 0
+    #         dp[(i, curr_target)] = dfs(i + 1, curr_target - nums[i]) + dfs(
+    #             i + 1, curr_target + nums[i]
+    #         )
+    #         return dp[(i, curr_target)]
+
+    #     return dfs(0, target)
 
     # # brute force - too slow
     # def findTargetSumWays(self, nums: List[int], target: int) -> int:
