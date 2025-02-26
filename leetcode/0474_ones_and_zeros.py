89cbcb9eab07baaa8048541f05adb171ad79f720
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-02-26 19:33:36 +0000

    fix top down dp approach - be consistent with updating cache

--- leetcode/0474_ones_and_zeros.py
+++ leetcode/0474_ones_and_zeros.py
@@ -1,45 +1,33 @@
-from collections import defaultdict
 from typing import List
 
 
 class Solution:
     # top down dp with cache
     def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
-        # Time O(2^n), Memory O(2^n)
-        dp = {}  # (index, #ones, #zeros): #solutions
+        # Time O(s*m*n), Memory O(s*m*n) where s=len(strs)
+        dp = {}  # (index, #zeros, #ones): #solutions
 
-        def backtrack(i, curr):
-            if (i, curr["1"], curr["0"]) in dp:
-                return dp[(i, curr["1"], curr["0"])]
+        def backtrack(i, zeros, ones):
             if i == len(strs):
-                if curr["0"] <= m and curr["1"] <= n:
-                    dp[(i, curr["1"], curr["0"])] = curr["size"]
-                    return curr["size"]
-                else:
-                    dp[(i, curr["1"], curr["0"])] = 0
-                    return 0
-
-            copy_ones = curr["1"]
-            copy_zeros = curr["0"]
+                return 0
+            if (i, zeros, ones) in dp:
+                return dp[(i, zeros, ones)]
 
             # don't include
-            r1 = backtrack(i + 1, curr)
+            r1 = backtrack(i + 1, zeros, ones)
 
             # include
-            ones = sum([1 if s == "1" else 0 for s in strs[i]])
-            zeros = len(strs[i]) - ones
-            curr["1"] += ones
-            curr["0"] += zeros
-            curr["size"] += 1
-            r2 = backtrack(i + 1, curr)
-            curr["1"] -= ones
-            curr["0"] -= zeros
-            curr["size"] -= 1
+            i_zeros = strs[i].count("0")
+            i_ones = strs[i].count("1")
+            if zeros + i_zeros <= m and ones + i_ones <= n:
+                r2 = backtrack(i + 1, zeros + i_zeros, ones + i_ones)
+            else:
+                r2 = -1
 
-            dp[(i, copy_ones, copy_zeros)] = max(r1, r2)
-            return dp[(i, curr["1"], curr["0"])]
+            dp[(i, zeros, ones)] = max(r1, 1 + r2)
+            return dp[(i, zeros, ones)]
 
-        return backtrack(0, defaultdict(int))
+        return backtrack(0, 0, 0)
 
     # # brute force on binary decision tree
     # def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
