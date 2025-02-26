67f5cf22b85987bd587bbe449ee69fef4cb242cc
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-02-26 20:02:20 +0000

    brute force creating new array each time

---
+++ leetcode/1049_last_stone_weight_ii.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class Solution:
+    # brute force
+    def lastStoneWeightII(self, stones: List[int]) -> int:
+        # Time O((n^2)^n), Memory O((n^2)^n)
+        def backtrack(arr, res):
+            if len(arr) == 1:
+                return arr[0]
+            elif len(arr) == 0:
+                return 0
+
+            new_arr = list(arr)
+            new_res = float("inf")
+            for i in range(len(arr)):
+                for j in range(i + 1, len(arr)):
+                    new_arr = list(arr)
+                    stone1 = new_arr.pop(i)
+                    stone2 = new_arr.pop(j - 1)
+                    if stone1 != stone2:
+                        new_arr.append(abs(stone2 - stone1))
+                    new_res = min(new_res, backtrack(new_arr, res))
+            return min(res, new_res)
+
+        return backtrack(stones, float("inf"))
