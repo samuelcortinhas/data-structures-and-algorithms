2477ce49945ec8165df642b589e33117a6c808ce
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-02-28 12:48:48 +0000

    bottom up dp solution

--- leetcode/0518_coin_change_ii.py
+++ leetcode/0518_coin_change_ii.py
@@ -2,24 +2,37 @@ from typing import List
 
 
 class Solution:
-    # Top down dp
+    # bottom up dp
     def change(self, amount: int, coins: List[int]) -> int:
         # Time O(n*m), Memory O(n*m), where n=amount, m=len(coins)
-        dp = {}
+        dp = [[0] * (len(coins) + 1) for _ in range(amount + 1)]
+        dp[0] = [1] * (len(coins) + 1)
 
-        def dfs(i, curr_amount):
-            if curr_amount == amount:
-                return 1
-            if curr_amount > amount:
-                return 0
-            if i == len(coins):
-                return 0
-            if (i, curr_amount) in dp:
-                return dp[(i, curr_amount)]
+        for i in range(1, amount + 1):
+            for j in range(len(coins) - 1, -1, -1):
+                dp[i][j] = dp[i][j + 1]
+                if i - coins[j] >= 0:
+                    dp[i][j] += dp[i - coins[j]][j]
+        return dp[amount][0]
 
-            dp[(i, curr_amount)] = dfs(i, curr_amount + coins[i]) + dfs(
-                i + 1, curr_amount
-            )
-            return dp[(i, curr_amount)]
+    # # Top down dp
+    # def change(self, amount: int, coins: List[int]) -> int:
+    #     # Time O(n*m), Memory O(n*m), where n=amount, m=len(coins)
+    #     dp = {}
 
-        return dfs(0, 0)
+    #     def dfs(i, curr_amount):
+    #         if curr_amount == amount:
+    #             return 1
+    #         if curr_amount > amount:
+    #             return 0
+    #         if i == len(coins):
+    #             return 0
+    #         if (i, curr_amount) in dp:
+    #             return dp[(i, curr_amount)]
+
+    #         dp[(i, curr_amount)] = dfs(i, curr_amount + coins[i]) + dfs(
+    #             i + 1, curr_amount
+    #         )
+    #         return dp[(i, curr_amount)]
+
+    #     return dfs(0, 0)
