8396dcc917fc2147acc1b462a3a29fb1e71ec234
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-02-28 15:05:10 +0000

    top down memo solution

--- leetcode/0097_interleaving_strings.py
+++ leetcode/0097_interleaving_strings.py
@@ -1,11 +1,15 @@
 class Solution:
-    # brute force
+    # top down dp with memoisation
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        # Time O(2^n), Memory O(2^n) where n=len(s3)
+        # Time O(n*m), Memory O(n*m) where n=len(s1), m=len(s2)
         if len(s1) + len(s2) != len(s3):
             return False
 
+        dp = {}
+
         def backtrack(i, j):
+            if (i, j) in dp:
+                return dp[(i, j)]
             if i == len(s1) and j == len(s2):
                 return True
             elif i == len(s1):
@@ -20,12 +24,44 @@ class Solution:
                     return False
 
             if s1[i] == s3[i + j] and s2[j] != s3[i + j]:
-                return backtrack(i + 1, j)
+                dp[(i, j)] = backtrack(i + 1, j)
             elif s1[i] != s3[i + j] and s2[j] == s3[i + j]:
-                return backtrack(i, j + 1)
+                dp[(i, j)] = backtrack(i, j + 1)
             elif s1[i] == s3[i + j] and s2[j] == s3[i + j]:
-                return backtrack(i + 1, j) or backtrack(i, j + 1)
+                dp[(i, j)] = backtrack(i + 1, j) or backtrack(i, j + 1)
             else:
-                return False
+                dp[(i, j)] = False
+            return dp[(i, j)]
 
         return backtrack(0, 0)
+
+    # # brute force
+    # def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+    #     # Time O(2^n), Memory O(2^n) where n=len(s3)
+    #     if len(s1) + len(s2) != len(s3):
+    #         return False
+
+    #     def backtrack(i, j):
+    #         if i == len(s1) and j == len(s2):
+    #             return True
+    #         elif i == len(s1):
+    #             if s2[j] == s3[i + j]:
+    #                 return backtrack(i, j + 1)
+    #             else:
+    #                 return False
+    #         elif j == len(s2):
+    #             if s1[i] == s3[i + j]:
+    #                 return backtrack(i + 1, j)
+    #             else:
+    #                 return False
+
+    #         if s1[i] == s3[i + j] and s2[j] != s3[i + j]:
+    #             return backtrack(i + 1, j)
+    #         elif s1[i] != s3[i + j] and s2[j] == s3[i + j]:
+    #             return backtrack(i, j + 1)
+    #         elif s1[i] == s3[i + j] and s2[j] == s3[i + j]:
+    #             return backtrack(i + 1, j) or backtrack(i, j + 1)
+    #         else:
+    #             return False
+
+    #     return backtrack(0, 0)
