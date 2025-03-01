fa4b8b29c6a9588617f7b41745f9f742c0aa9ef2
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-03-01 12:24:30 +0000

    honestly no clue how this works

--- leetcode/0312_burst_ballons.py
+++ leetcode/0312_burst_ballons.py
@@ -2,25 +2,46 @@ from typing import List
 
 
 class Solution:
-    # brute force - way too slow
+    # dp solution - keep track of left and right boundaries for each subarray
     def maxCoins(self, nums: List[int]) -> int:
-        # Time O(n!), Memory O(n!)
-        return self.dfs(nums)
+        # Time O(n^3), Memory O(n^2)
+        nums = [1] + nums + [1]
+        dp = {}
 
-    def dfs(self, nums):
-        best_score = 0
-        for i in range(len(nums)):
-            if i - 1 >= 0 and i + 1 < len(nums):
-                score = nums[i - 1] * nums[i] * nums[i + 1]
-            elif i - 1 >= 0:
-                score = nums[i - 1] * nums[i]
-            elif i + 1 < len(nums):
-                score = nums[i] * nums[i + 1]
-            else:
-                score = nums[i]
-            nxt = list(nums)
-            nxt.pop(i)
-            score += self.dfs(nxt)
-            if score > best_score:
-                best_score = score
-        return best_score
+        def dfs(l, r):
+            if l > r:
+                return 0
+            if (l, r) in dp:
+                return dp[(l, r)]
+
+            dp[(l, r)] = 0
+            for i in range(l, r + 1):
+                coins = nums[l - 1] * nums[i] * nums[r + 1]
+                coins += dfs(l, i - 1) + dfs(i + 1, r)
+                dp[(l, r)] = max(dp[(l, r)], coins)
+            return dp[(l, r)]
+
+        return dfs(1, len(nums) - 2)
+
+    # # brute force - way too slow
+    # def maxCoins(self, nums: List[int]) -> int:
+    #     # Time O(n!), Memory O(n!)
+    #     return self.dfs(nums)
+
+    # def dfs(self, nums):
+    #     best_score = 0
+    #     for i in range(len(nums)):
+    #         if i - 1 >= 0 and i + 1 < len(nums):
+    #             score = nums[i - 1] * nums[i] * nums[i + 1]
+    #         elif i - 1 >= 0:
+    #             score = nums[i - 1] * nums[i]
+    #         elif i + 1 < len(nums):
+    #             score = nums[i] * nums[i + 1]
+    #         else:
+    #             score = nums[i]
+    #         nxt = list(nums)
+    #         nxt.pop(i)
+    #         score += self.dfs(nxt)
+    #         if score > best_score:
+    #             best_score = score
+    #     return best_score
