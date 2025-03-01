4d7960a886c351d12300c8fd3be091eea2c69cbf
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-03-01 19:35:17 +0000

    reverse integer

--- leetcode/0009_palindrome_number.py
+++ leetcode/0009_palindrome_number.py
@@ -1,12 +1,29 @@
 class Solution:
-    # convert to string
+    # reverse number
     def isPalindrome(self, x: int) -> bool:
-        # Time O(n), Memory O(1)
-        x = str(x)
-        i, j = 0, len(x) - 1
-        while i <= j:
-            if x[i] != x[j]:
-                return False
+        # Time O(log n), Memory O(log n)
+        if x < 0:
+            return False
+        if x == 0:
+            return True
+
+        rev = 0
+        y = x
+        i = 0
+        while y:
+            rev = 10 * rev + (y % 10)
+            y = y // 10
             i += 1
-            j -= 1
-        return True
+        return x == rev
+
+    # # convert to string
+    # def isPalindrome(self, x: int) -> bool:
+    #     # Time O(log n), Memory O(log n)
+    #     x = str(x)
+    #     i, j = 0, len(x) - 1
+    #     while i <= j:
+    #         if x[i] != x[j]:
+    #             return False
+    #         i += 1
+    #         j -= 1
+    #     return True
