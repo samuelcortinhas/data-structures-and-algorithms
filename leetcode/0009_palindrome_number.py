399ac26bdb2ac5fd5be61a968e5ee6c74d8c965e
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-03-01 19:36:44 +0000

    optimise code

--- leetcode/0009_palindrome_number.py
+++ leetcode/0009_palindrome_number.py
@@ -4,16 +4,12 @@ class Solution:
         # Time O(log n), Memory O(log n)
         if x < 0:
             return False
-        if x == 0:
-            return True
 
         rev = 0
         y = x
-        i = 0
         while y:
             rev = 10 * rev + (y % 10)
             y = y // 10
-            i += 1
         return x == rev
 
     # # convert to string
