baaf77edf23143513e8de8bc5468359f455ca0f9
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-03-02 22:07:29 +0000

    compute sign only

--- leetcode/1822_sign_of_the_product_of_an_array.py
+++ leetcode/1822_sign_of_the_product_of_an_array.py
@@ -2,14 +2,26 @@ from typing import List
 
 
 class Solution:
+    # compute sign
     def arraySign(self, nums: List[int]) -> int:
         # Time O(n), Memory O(1)
-        prod = 1
+        res = 1
         for n in nums:
-            prod *= n
-        if prod > 0:
-            return 1
-        elif prod < 0:
-            return -1
-        else:
-            return 0
+            if n == 0:
+                return 0
+            if n < 0:
+                res *= -1
+        return res
+
+    # # compute product
+    # def arraySign(self, nums: List[int]) -> int:
+    #     # Time O(n), Memory O(1)
+    #     prod = 1
+    #     for n in nums:
+    #         prod *= n
+    #     if prod > 0:
+    #         return 1
+    #     elif prod < 0:
+    #         return -1
+    #     else:
+    #         return 0
