e1d34b05a978a1ff870d0be9b9383a2ad151535a
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-03-02 09:34:46 +0000

    two pointer solution similar to merge sort

---
+++ leetcode/2570_merge_two_2d_arrays_by_summing_values.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+class Solution:
+    def mergeArrays(
+        self, nums1: List[List[int]], nums2: List[List[int]]
+    ) -> List[List[int]]:
+        # Time O(m+n), Memory O(m+n)
+        i, j = 0, 0
+        res = []
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i][0] == nums2[j][0]:
+                res.append([nums1[i][0], nums1[i][1] + nums2[j][1]])
+                i += 1
+                j += 1
+            elif nums1[i][0] < nums2[j][0]:
+                res.append(nums1[i])
+                i += 1
+            else:
+                res.append(nums2[j])
+                j += 1
+
+        res.extend(nums1[i:])
+        res.extend(nums2[j:])
+        return res
