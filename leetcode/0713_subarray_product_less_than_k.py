e2a0658b554987a492def515d392088825bc6b9a
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-03-03 20:34:25 +0000

    initial attempt uses too much memory

---
+++ leetcode/0713_subarray_product_less_than_k.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+class Solution:
+    # compute prefix products - uses too much memory
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        # Time O(n^2), Memory O(n^2)
+        left, right = 0, 0
+        res = 0
+        pre = [[1] * (len(nums) + 1) for _ in range(len(nums))]
+        for i in range(len(nums)):
+            for j in range(1, len(nums) + 1):
+                if j > i:
+                    pre[i][j] = nums[j - 1] * pre[i][j - 1]
+
+        while left < len(nums):
+            right = left
+            p = pre[left][right + 1]
+            while right < len(nums):
+                p = pre[left][right + 1]
+                if p < k:
+                    res += 1
+                    right += 1
+                else:
+                    break
+            left += 1
+        return res
