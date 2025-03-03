5672d8ba5565fa32d82c5247753ab5e3eaf5a9c7
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-03-03 10:18:40 +0000

    2 pointer memory optimised solution

--- leetcode/2161_partition_array_according_to_given_pivot.py
+++ leetcode/2161_partition_array_according_to_given_pivot.py
@@ -2,14 +2,32 @@ from typing import List
 
 
 class Solution:
+    # 2 pointer - memory optimised solution
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
-        # Time O(n), Memory O(n)
-        less, more = [], []
-        for n in nums:
-            if n < pivot:
-                less.append(n)
-            elif n > pivot:
-                more.append(n)
+        # Time O(n), Memory O(1)
+        res = [pivot] * len(nums)
+        left, right = 0, len(nums) - 1
+        i, j = 0, len(nums) - 1
+        while j >= 0:
+            if nums[i] < pivot:
+                res[left] = nums[i]
+                left += 1
+            if nums[j] > pivot:
+                res[right] = nums[j]
+                right -= 1
+            i += 1
+            j -= 1
+        return res
 
-        n = len(nums) - len(less) - len(more)
-        return less + [pivot] * n + more
+    # # split into arrays
+    # def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+    #     # Time O(n), Memory O(n)
+    #     less, more = [], []
+    #     for n in nums:
+    #         if n < pivot:
+    #             less.append(n)
+    #         elif n > pivot:
+    #             more.append(n)
+
+    #     n = len(nums) - len(less) - len(more)
+    #     return less + [pivot] * n + more
