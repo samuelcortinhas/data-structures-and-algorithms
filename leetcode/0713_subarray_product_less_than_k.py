18921bebbe826287cf86868179068f30ae770bac
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-03-03 20:36:53 +0000

    second attempt is too slow

--- leetcode/0713_subarray_product_less_than_k.py
+++ leetcode/0713_subarray_product_less_than_k.py
@@ -2,22 +2,16 @@ from typing import List
 
 
 class Solution:
-    # compute prefix products - uses too much memory
+    # compute prefix products on demand - too slow
     def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
-        # Time O(n^2), Memory O(n^2)
+        # Time O(n^2), Memory O(1)
         left, right = 0, 0
         res = 0
-        pre = [[1] * (len(nums) + 1) for _ in range(len(nums))]
-        for i in range(len(nums)):
-            for j in range(1, len(nums) + 1):
-                if j > i:
-                    pre[i][j] = nums[j - 1] * pre[i][j - 1]
-
         while left < len(nums):
             right = left
-            p = pre[left][right + 1]
+            p = 1
             while right < len(nums):
-                p = pre[left][right + 1]
+                p *= nums[right]
                 if p < k:
                     res += 1
                     right += 1
@@ -25,3 +19,27 @@ class Solution:
                     break
             left += 1
         return res
+
+    # compute prefix products - uses too much memory
+    # def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+    #     # Time O(n^2), Memory O(n^2)
+    #     left, right = 0, 0
+    #     res = 0
+    #     pre = [[1] * (len(nums) + 1) for _ in range(len(nums))]
+    #     for i in range(len(nums)):
+    #         for j in range(1, len(nums) + 1):
+    #             if j > i:
+    #                 pre[i][j] = nums[j - 1] * pre[i][j - 1]
+
+    #     while left < len(nums):
+    #         right = left
+    #         p = pre[left][right + 1]
+    #         while right < len(nums):
+    #             p = pre[left][right + 1]
+    #             if p < k:
+    #                 res += 1
+    #                 right += 1
+    #             else:
+    #                 break
+    #         left += 1
+    #     return res
