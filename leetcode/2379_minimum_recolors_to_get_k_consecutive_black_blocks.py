c54fa5c4cc131979c5e6b3a1555a6b615dbc246b
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-03-08 18:43:42 +0000

    optimise with .count

--- leetcode/2379_minimum_recolors_to_get_k_consecutive_black_blocks.py
+++ leetcode/2379_minimum_recolors_to_get_k_consecutive_black_blocks.py
@@ -1,10 +1,9 @@
 class Solution:
     def minimumRecolors(self, blocks: str, k: int) -> int:
         # Time O(n), Memory O(1)
-        curr = sum([1 for b in blocks[:k] if b == "B"])
+        curr = blocks[:k].count("B")
         max_num = curr
         for i in range(k, len(blocks)):
-            curr += blocks[i] == "B"
-            curr -= blocks[i - k] == "B"
+            curr += (blocks[i] == "B") - (blocks[i - k] == "B")
             max_num = max(max_num, curr)
         return max(k - max_num, 0)
