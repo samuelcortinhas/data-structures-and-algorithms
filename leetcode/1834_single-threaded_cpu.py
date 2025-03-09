e8747028e9945d51a084b8dfd865fa7f646de2c4
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-03-09 22:47:47 +0000

    2 heap solution for tasks and batch

--- leetcode/1834_single-threaded_cpu.py
+++ leetcode/1834_single-threaded_cpu.py
@@ -3,9 +3,9 @@ from typing import List
 
 
 class Solution:
-    # a little better
+    # 2 heap solution
     def getOrder(self, tasks: List[List[int]]) -> List[int]:
-        # Time O(n^2), Memory O(n)
+        # Time O(n log n), Memory O(n)
         curr_time = 0
         heap = [(t, d, i) for i, (t, d) in enumerate(tasks)]
         heapq.heapify(heap)
@@ -15,21 +15,41 @@ class Solution:
             if not batch:
                 curr_time = max(curr_time, heap[0][0] if heap else 0)
             while heap and heap[0][0] <= curr_time:
-                batch.append(heapq.heappop(heap))
+                nxt = heapq.heappop(heap)
+                heapq.heappush(batch, (nxt[1], nxt[2]))
 
-            shortest = float("inf")
-            index = float("inf")
-            del_k = None
-            for k, b in enumerate(batch):
-                if b[1] < shortest or (b[1] == shortest and b[2] < index):
-                    shortest = b[1]
-                    index = b[2]
-                    del_k = k
-
-            curr_time += shortest
-            batch.pop(del_k)
-            res.append(index)
+            d, i = heapq.heappop(batch)
+            curr_time += d
+            res.append(i)
         return res
+
+    # # a little better
+    # def getOrder(self, tasks: List[List[int]]) -> List[int]:
+    #     # Time O(n^2), Memory O(n)
+    #     curr_time = 0
+    #     heap = [(t, d, i) for i, (t, d) in enumerate(tasks)]
+    #     heapq.heapify(heap)
+    #     res = []
+    #     batch = []
+    #     while heap or batch:
+    #         if not batch:
+    #             curr_time = max(curr_time, heap[0][0] if heap else 0)
+    #         while heap and heap[0][0] <= curr_time:
+    #             batch.append(heapq.heappop(heap))
+
+    #         shortest = float("inf")
+    #         index = float("inf")
+    #         del_k = None
+    #         for k, b in enumerate(batch):
+    #             if b[1] < shortest or (b[1] == shortest and b[2] < index):
+    #                 shortest = b[1]
+    #                 index = b[2]
+    #                 del_k = k
+
+    #         curr_time += shortest
+    #         batch.pop(del_k)
+    #         res.append(index)
+    #     return res
 
     # # initial attempt - too slow
     # def getOrder(self, tasks: List[List[int]]) -> List[int]:
