90cd01285de0cbc4ebfdc3f9771344b300eb6ab5
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2025-03-11 11:58:23 +0000

    linear time sliding window, right+=1, left+=1 if min(counts) > 0

--- leetcode/1358_number_of_substrings_containing_all_three_characters.py
+++ leetcode/1358_number_of_substrings_containing_all_three_characters.py
@@ -1,17 +1,31 @@
 class Solution:
-    # sliding window - tle
+    # sliding window - linear time
     def numberOfSubstrings(self, s: str) -> int:
-        # Time O(n^2), Memory O(1)
+        # Time O(n), Memory O(1)
         res = 0
-        for left in range(len(s) - 2):
-            counts = [0] * 3
-            for right in range(left, left + 3):
-                counts[ord(s[right]) - 97] += 1
-
-            while min(counts) == 0 and right < len(s) - 1:
-                right += 1
-                counts[ord(s[right]) - 97] += 1
-
-            if min(counts) > 0:
+        counts = [0] * 3
+        left = 0
+        for right in range(len(s)):
+            counts[ord(s[right]) - 97] += 1
+            while min(counts) > 0:
                 res += len(s) - right
+                counts[ord(s[left]) - 97] -= 1
+                left += 1
         return res
+
+    # # sliding window - tle
+    # def numberOfSubstrings(self, s: str) -> int:
+    #     # Time O(n^2), Memory O(1)
+    #     res = 0
+    #     for left in range(len(s) - 2):
+    #         counts = [0] * 3
+    #         for right in range(left, left + 3):
+    #             counts[ord(s[right]) - 97] += 1
+
+    #         while min(counts) == 0 and right < len(s) - 1:
+    #             right += 1
+    #             counts[ord(s[right]) - 97] += 1
+
+    #         if min(counts) > 0:
+    #             res += len(s) - right
+    #     return res
